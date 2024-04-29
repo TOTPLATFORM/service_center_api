0dfa5c32cd70382e566d04d146b65c69d901f808
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 20:55:36 +0300

    add service category request dto , service category response dto

---
+++ ServiceCenter.Application/DTOS/ServiceCategoryRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class ServiceCategoryRequestDto
+    {
+        [Required]
+        public string ServiceCategoryName { get; set; } = "";
+        [Required]
+        public string ServiceCategoryDescription { get; set; } = "";
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
