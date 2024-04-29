1d970a6d50dfa4161e645ca02d3a79789c16c223
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 14:10:29 +0300

    add branch request dto , add branch response

---
+++ ServiceCenter.Application/DTOS/BranchRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class BranchRequestDto
+    {
+        [Required]
+        public string BranchName { get; set; } = "";
+        [Required]
+        public Address Address { get; set; }
+        [Required]
+        public string BranchPhoneNumber { get; set; } = "";
+        [Required]
+        public string EmailAddress { get; set; } = "";
+        [Required]
+        public int CenterId { get; set; }
+        [Required]
+        public string CustomerId { get; set; } = "";
+        
+
+    }
+}
---
+++ ServiceCenter.Application/DTOS/BranchResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class BranchResponseDto
+    {
+        public int Id { get; set; }
+        public string BranchName { get; set; } = "";
+        public string City { get; set; } = "";
+        public string Country { get; set; } = "";
+        public string PostalCode { get; set; } = "";
+        public string BranchPhoneNumber { get; set; } = "";
+        public string EmailAddress { get; set; } = "";
+        public string CenterName { get; set; } = "";
+
+
+      
+    }
+}
