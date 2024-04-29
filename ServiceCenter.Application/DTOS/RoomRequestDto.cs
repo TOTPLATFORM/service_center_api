5c2e89259484f1ad1989520ebd4010227f66c14d
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 19:56:19 +0300

    add room request dto ,room response dto

---
+++ ServiceCenter.Application/DTOS/RoomRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class RoomRequestDto
+    {
+        [Required]
+        public int RoomNumber { get; set; }
+        [Required]
+        public bool Availability { get; set; }
+        [Required]
+        public int CenterId { get; set; }
+    }
+}
