5fd6ba048cc5329579c31d87ada3405161dfad17
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 00:11:37 +0300

    add add product brand function in product brand service

---
+++ ServiceCenter.Application/Contracts/IProductBrandService.cs
@@ -0,0 +1,21 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts
+{
+    public interface IProductBrandService : IApplicationService, IScopedService
+    {
+        /// <summary>
+        /// function to add  product brand that take  productBrandDto   
+        /// </summary>
+        /// <param name=" productBrandRequestDto">product brand request dto</param>
+        /// <returns> product brand added successfully </returns>
+        public Task<Result> AddProductBrandAsync( ProductBrandRequestDto  productBrandRequestDto);
+
+    }
+}
---
+++ ServiceCenter.Application/Services/ProductBrandService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services
+{
+    public class ProductBrandService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<ProductBrandService> logger, IUserContextService userContext) : IProductBrandService
+    {
+        private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+        private readonly ILogger<ProductBrandService> _logger = logger;
+        private readonly IUserContextService _userContext = userContext;
+
+        ///<inheritdoc/>
+        public async Task<Result> AddProductBrandAsync(ProductBrandRequestDto productBrandRequestDto)
+        {
+            var result = _mapper.Map<ProductBrand>(productBrandRequestDto);
+            if (result is null)
+            {
+                _logger.LogError("Failed to map ProductBrandRequestDto to ProductBrand. ProductBrandRequestDto: {@ProductBrandRequestDto}", productBrandRequestDto);
+                return Result.Invalid(new List<ValidationError>
+            {
+                new ValidationError
+                {
+                    ErrorMessage = "Validation Errror"
+                }
+            });
+            }
+            result.CreatedBy = _userContext.Email;
+            _dbContext.ProductBrands.Add(result);
+            await _dbContext.SaveChangesAsync();
+            _logger.LogInformation("ProductBrand added successfully to the database");
+            return Result.SuccessWithMessage("ProductBrand added successfully");
+        }
+
+
+
+
+
+    }
+}
