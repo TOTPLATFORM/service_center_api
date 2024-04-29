c3edd898ec72b332a0313f26df3005e055b9cb4d
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 14:08:47 +0300

    add complaint request dto ,complaint response dto

---
+++ ServiceCenter.Application/DTOS/ComplaintRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using ServiceCenter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class ComplaintRequestDto
+    {
+        [Required]
+        public DateOnly ComplaintDate { get; set; }
+        [Required]
+        public string ComplaintDescription { get; set; } = "";
+        [Required]
+        public string ComplaintCategory { get; set; } = "";
+        [Required]
+        public Status ComplaintStatus { get; set; }
+        [Required]
+        public string AssignedTo { get; set; } = "";
+        [Required]
+        public string CustomerId { get; set; } = "";
+    }
+}
---
+++ ServiceCenter.Application/DTOS/ComplaintResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using ServiceCenter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class ComplaintResponseDto
+    {
+        public int Id { get; set; }
+        public DateOnly ComplaintDate { get; set; }
+        public string ComplaintDescription { get; set; } = "";
+        public string ComplaintCategory { get; set; } = "";
+        public Status ComplaintStatus { get; set; }
+        public string AssignedTo { get; set; } = "";
+        public string CustomerName { get; set; } = "";
+    }
+}
