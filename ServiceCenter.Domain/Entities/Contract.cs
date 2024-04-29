2ee22e55ea3b7c65b7edc9a7a359ba4ea73e09b6
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 21:56:47 +0300

    add contract request dto , contract response dto

--- ServiceCenter.Domain/Entities/Contract.cs
+++ ServiceCenter.Domain/Entities/Contract.cs
@@ -9,6 +9,7 @@ namespace ServiceCenter.Domain.Entities;
     
 public class Contract : AuditableEntity
 {
+    public  string Duration { get; set; } = "";
     public int ServicePackageId { get; set; }
     public virtual ServicePackage ServicePackage { get; set; }
 }
