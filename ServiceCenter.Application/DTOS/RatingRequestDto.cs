8e6b143b489fd081ebb2b564f8bf27877e277bce
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 14:44:24 +0300

    add rating request dto ,rating response dto

---
+++ ServiceCenter.Application/DTOS/RatingRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class RatingRequestDto
+    {
+        [Required]
+        public int RatingValue { get; set; }
+        [Required]
+        public DateOnly RatingDate { get; set; }
+
+    }
+}
