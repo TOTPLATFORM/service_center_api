f7cc3cbfd95deefda70570a40c8c247bcb103087
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 14:58:26 +0300

    add offer request dto , add offer response dto

---
+++ ServiceCenter.Application/DTOS/OfferResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class OfferResponseDto
+    {
+        public int Id { get; set; }
+        public string OfferName { get; set; } = "";
+        public string OfferDescription { get; set; } = "";
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public int Discount { get; set; }
+    }
+}
