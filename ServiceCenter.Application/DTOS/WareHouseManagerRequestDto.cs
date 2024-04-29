f7123eeea1633d6f8a5fd407b692181408652d04
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 21:16:23 +0300

    add warehousemanager request dto,warehousemanager response dto

---
+++ ServiceCenter.Application/DTOS/WareHouseManagerRequestDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class WareHouseManagerRequestDto
+    {
+        [Required]
+        public string WareHouseManagerEmail { get; set; } = "";
+        [Required]
+        public string WareHouseManagerFirstName { get; set; } = "";
+        [Required]
+        public string WareHouseManagerLastName { get; set; } = "";
+        [Required]
+        public string WareHouseManagerPhoneNumber { get; set; } = "";
+        [Required]
+        public string UserName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public DateOnly DateOfBirth { get; set; }
+        public string Gender { get; set; } = "";
+        [Required]
+        public string PositionTitle { get; set; } = "";
+        [Required]
+        public DateOnly StartDate { get; set; }
+        [Required]
+        public DateOnly EndtDate { get; set; }
+        [Required]
+        public int InventoryId { get; set; }
+    }
+}
