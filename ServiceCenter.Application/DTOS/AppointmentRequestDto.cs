548c18a5fb62326ba3c37f84d463598f5d4bc898
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 14:07:31 +0300

    add appointment request dto ,add appointment response dto

---
+++ ServiceCenter.Application/DTOS/AppointmentRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class AppointmentRequestDto
+    {
+        [Required]
+        public TimeOnly StartTime { get; set; }
+        [Required]
+        public TimeOnly EndTime { get; set; }
+        [Required]
+        public DateOnly Date { get; set; }
+       [Required]
+        public string CustomerId { get; set; } = "";
+        [Required]
+        public string EmployeeId { get; set; } = "";
+    }
+}
---
+++ ServiceCenter.Application/DTOS/AppointmentResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class AppointmentResponseDto
+    {
+        public int Id { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public DateOnly Date { get; set; }
+        public string CustomerName { get; set; } = "";
+        public string EmployeeName { get; set; } = "";
+    }
+}
