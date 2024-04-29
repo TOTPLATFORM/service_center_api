87b2621fe504c125851864b7a022071991bd9e77
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 12:24:10 +0300

    add inventory

---
+++ ServiceCenter.Domain/Entities/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using ServiceCenter.Core.Entities;
+
+namespace ServiceCenter.Domain.Entities;
+
+public class Inventory : AuditableEntity
+{
+	public string InventoryName { get; set; } = "";
+	public string InventoryLocation { get; set; } = "";
+	public int InventoryCapacity { get; set; }
+	public virtual ICollection<ItemCategory> Categories { get; set; } = new HashSet<ItemCategory>();
+}
