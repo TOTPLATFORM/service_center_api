043b040cf00b7b941727526c37ceed6cc4668258
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 20:31:54 +0300

    add service request dto ,service response dto

---
+++ ServiceCenter.Application/DTOS/ServiceRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using ServiceCenter.Domain.Entities;
+using ServiceCenter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class ServiceRequestDto
+    {
+        [Required]
+        public string ServiceName { get; set; } = "";
+        [Required]
+        public string ServiceDescription { get; set; } = "";
+        [Required]
+        public int ServicePrice { get; set; }
+        [Required]
+        public Status Avaliable { get; set; }
+        [Required]
+        public int ServiceCategoryId { get; set; }
+        [Required]
+        public int ServicePackageId { get; set; }
+        [Required]
+        public string EmployeeId { get; set; } = "";
+}
