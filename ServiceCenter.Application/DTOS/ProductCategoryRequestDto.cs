eb955e159d25d9773096507ce68e88ac9600b7d4
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 19:26:41 +0300

    add product category request dto ,product category response dto

---
+++ ServiceCenter.Application/DTOS/ProductCategoryRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class ProductCategoryRequestDto
+    {
+        [Required]
+        public string CategoryName { get; set; } = "";
+        [Required]
+        public int ReferenceNumber { get; set; }
+    }
+}
