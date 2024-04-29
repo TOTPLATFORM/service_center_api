abc9ed7666c00cf8e1bd1b87dadfd11e835d518e
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 18:43:53 +0300

    add inventory request dto ,inventory response dto

---
+++ ServiceCenter.Application/DTOS/InventoryResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class InventoryResponseDto
+    {
+        public int Id { get; set; }
+        public string InventoryName { get; set; } = "";
+        public string InventoryLocation { get; set; } = "";
+        public int InventoryCapacity { get; set; }
+
+        public ICollection<ItemCategoryResponeDto> Categories { get; set; }
+    }
+}
