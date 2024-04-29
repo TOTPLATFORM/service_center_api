3200201c7b810b67b738089d8e100753490eb180
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-29 19:53:13 +0300

    add product brand request dto ,product brand response dto

---
+++ ServiceCenter.Application/DTOS/ProductBrandResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS
+{
+    public class ProductBrandResponseDto
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; } = "";
+        public string BrandDescription { get; set; } = "";
+        public string CountryOfOrigin { get; set; } = "";
+        public DateOnly FoundedYear { get; set; }
+    }
+}
