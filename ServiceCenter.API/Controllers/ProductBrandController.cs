8d1f2469223b757eae62439c9099082cf700ca6b
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 00:12:14 +0300

    add add product brand action in product brand controller

---
+++ ServiceCenter.API/Controllers/ProductBrandController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+
+namespace ServiceCenter.API.Controllers
+{
+
+    public class ProductBrandController(IProductBrandService productBrandService) : BaseController
+    {
+        private readonly IProductBrandService _productBrandService = productBrandService;
+
+        /// <summary>
+        /// action for add product brand action that take  productBrand dto   
+        /// </summary>
+        /// <param name="productBrandDto">product brand dto</param>
+        /// <returns>result for product brand added successfully.</returns>
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        [ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+        public async Task<Result> AddProductBrand(ProductBrandRequestDto productBrandDto)
+        {
+            return await _productBrandService.AddProductBrandAsync(productBrandDto);
+        }
+    }
+    }
