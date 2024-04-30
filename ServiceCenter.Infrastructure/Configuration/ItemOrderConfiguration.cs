72767103059f15b69d17246b1d3f6131dfb8f828
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 14:17:16 +0300

    add item order configuration

---
+++ ServiceCenter.Infrastructure/Configuration/ItemOrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class ItemOrderConfigurations : IEntityTypeConfiguration<ItemOrder>
+{
+    public void Configure(EntityTypeBuilder<ItemOrder> builder)
+    {
+        builder.Property(p => p.Quantity)
+              .IsRequired();
+
+        builder.Property(p => p.ItemId)
+               .IsRequired();
+
+    }
+}
