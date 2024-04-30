1bffea1da7fb811c7df3ff4f863d8f4503c31467
RadwaaAyman <radwa.ayman@totplatform.com>
2024-04-30 14:10:06 +0300

    add configuration for inventory

--- ServiceCenter.Application/Services/InventoryService.cs
+++ ServiceCenter.Application/Services/InventoryService.cs
@@ -30,7 +30,7 @@ public class InventoryService(ServiceCenterBaseDbContext dbContext, IMapper mapp
 
 		if (result is null)
 		{
-			_logger.LogError("Failed to map InventoryRequestDto to TimeSlot. InventoryRequestDto: {@InevntoryRequestDto}", inventoryRequestDto);
+			_logger.LogError("Failed to map InventoryRequestDto to Inventory. InventoryRequestDto: {@InevntoryRequestDto}", inventoryRequestDto);
 
 			return Result.Invalid(new List<ValidationError>
 			{
@@ -57,7 +57,7 @@ public class InventoryService(ServiceCenterBaseDbContext dbContext, IMapper mapp
 				 .ProjectTo<InventoryResponseDto>(_mapper.ConfigurationProvider)
 				 .ToListAsync();
 
-		_logger.LogInformation("Fetching all Inventories. Total count: {TimeSlot}.", result.Count);
+		_logger.LogInformation("Fetching all Inventories. Total count: {Inventory}.", result.Count);
 
 		return Result.Success(result);
 	}
@@ -78,5 +78,93 @@ public class InventoryService(ServiceCenterBaseDbContext dbContext, IMapper mapp
 		_logger.LogInformation("Fetching Inventory");
 
 		return Result.Success(result);
+	}
+
+	///<inheritdoc/>
+
+	public async Task<Result<InventoryResponseDto>> UpdateInventoryAsync(int id, InventoryRequestDto timeSlotRequestDto)
+	{
+		var result = await _dbContext.Inventories.FindAsync(id);
+
+		if (result is null)
+		{
+			_logger.LogWarning("Inventory Id not found,Id {InventoryId}", id);
+			return Result.NotFound(["Inventory not found"]);
+		}
+
+		result.ModifiedBy = _userContext.Email;
+
+		_mapper.Map(timeSlotRequestDto, result);
+
+		await _dbContext.SaveChangesAsync();
+
+		var inventory = _mapper.Map<InventoryResponseDto>(result);
+
+		if (inventory is null)
+		{
+			_logger.LogError("Failed to map InventoryRequestDto to InventoryResponseDto. InventoryRequestDto: {@InventoryRequestDto}", inventory);
+
+			return Result.Invalid(new List<ValidationError>
+			{
+					new ValidationError
+					{
+						ErrorMessage = "Validation Errror"
+					}
+			});
+		}
+
+		_logger.LogInformation("Updated Inventory , Id {Id}", id);
+
+		return Result.Success(inventory);
+	}
+
+	///<inheritdoc/>
+	public async Task<Result<List<InventoryResponseDto>>> SearchInventoryByTextAsync(string text)
+	{
+
+
+		//if (string.IsNullOrWhiteSpace(text))
+		//{
+		//	_logger.LogError("Search text cannot be empty", text);
+
+		//	return new Result.Invalid(new List<ValidationError>
+		//	{
+		//		new ValidationError
+		//		{
+		//			ErrorMessage = "Validation Errror : Search text cannot be empty"
+		//		}
+		//	});
+		//}
+
+		var Days = await _dbContext.Inventories
+					   .ProjectTo<InventoryResponseDto>(_mapper.ConfigurationProvider)
+					   .Where(n => n.InventoryName.Contains(text))
+					   .ToListAsync();
+
+		_logger.LogInformation("Fetching search time slot by name . Total count: {time slot}.", Days.Count);
+
+		return Result.Success(Days);
+
+	}
+
+	///<inheritdoc/>
+	public async Task<Result> DeleteInventoryAsync(int id)
+	{
+		var timeSlot = await _dbContext.Inventories.FindAsync(id);
+
+		if (timeSlot is null)
+		{
+			_logger.LogWarning("Inventory Invaild Id ,Id {InventoryId}", id);
+
+			return Result.NotFound(["Inventory Invaild Id"]);
+		}
+
+		_dbContext.Inventories.Remove(timeSlot);
+
+		await _dbContext.SaveChangesAsync();
+
+		_logger.LogInformation("Inventory removed successfully in the database");
+
+		return Result.SuccessWithMessage("Inventory removed successfully");
 	}
 }
