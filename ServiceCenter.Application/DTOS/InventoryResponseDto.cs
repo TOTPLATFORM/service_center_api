29b84562854a1ff8a827923f795f18f73236a889
RadwaaAyman <radwa.ayman@totplatform.com>
2024-04-30 14:08:14 +0300

    add update inventory fucntion in inventory service

--- ServiceCenter.Application/DTOS/InventoryResponseDto.cs
+++ ServiceCenter.Application/DTOS/InventoryResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceCenter.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,9 @@ namespace ServiceCenter.Application.DTOS
         public string InventoryName { get; set; } = "";
         public string InventoryLocation { get; set; } = "";
         public int InventoryCapacity { get; set; }
-        public string CategoryName { get; set; } = "";
-		public int ReferenceNumber { get; set; }
+        //public string CategoryName { get; set; } = "";
+        //public int ReferenceNumber { get; set; }
+        public ICollection<ItemCategory> ItemCategories { get; set; } =  new HashSet<ItemCategory>();
 
-	}
+    }
 }
