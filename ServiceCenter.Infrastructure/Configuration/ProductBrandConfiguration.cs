84baaf4b05c26b14d9384c9fa13aa762f308b77b
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 14:09:52 +0300

    add product brand configuration

---
+++ ServiceCenter.Infrastructure/Configuration/ProductBrandConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class ProductBrandConfiguration : IEntityTypeConfiguration<ProductBrand>
+{
+    public void Configure(EntityTypeBuilder<ProductBrand> builder)
+    {
+        builder.Property(T => T.BrandName)
+                    .IsRequired();
+        builder.Property(T => T.BrandDescription)
+               .IsRequired();
+    }
+}
