9115759187b4e77e935ad8365ce8febccf6902c4
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 15:40:27 +0300

    add get product category by id action in product category controller

--- ServiceCenter.API/Controllers/ProductCategoryController.cs
+++ ServiceCenter.API/Controllers/ProductCategoryController.cs
@@ -43,6 +43,22 @@ namespace ServiceCenter.API.Controllers
         {
             return await _productCategoryService.GetAllProductCategoryAsync();
         }
+        /// <summary>
+        /// get all inventories in the system.
+        /// </summary>
+        ///<param name="id">id of ProductCategory.</param>
+        /// <remarks>
+        /// Access is limited to users with the "Admin" role.
+        /// </remarks>
+        /// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+        [HttpGet("{id}")]
+        //[Authorize(Roles = "Admin")]
+        [ProducesResponseType(typeof(Result<ProductCategoryResponseDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+        public async Task<Result<ProductCategoryResponseDto>> GetProductCategoryById(int id)
+        {
+            return await _productCategoryService.GetProductCategoryByIdAsync(id);
+        }
 
     }
 }
