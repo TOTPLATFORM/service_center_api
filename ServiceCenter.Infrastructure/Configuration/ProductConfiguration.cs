2ff32854d9b3e145da58a285fe902f624141a182
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 16:10:25 +0300

    add product configuration

---
+++ ServiceCenter.Infrastructure/Configuration/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(T => T.ProductName)
+               .IsRequired();
+        builder.Property(T => T.ProductDescription)
+              .IsRequired();
+        builder.Property(T => T.ProductPrice)
+              .IsRequired();
+        builder.Property(T => T.CategoryId)
+              .IsRequired();
+        builder.Property(T => T.ProductBrandId)
+              .IsRequired();
+    }
+}
