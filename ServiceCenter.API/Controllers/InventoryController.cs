78a724d097a06100643289f50bbf55beca648ff6
RadwaaAyman <radwa.ayman@totplatform.com>
2024-04-30 11:47:12 +0300

    add get all inevntories action in  inventory controller

--- ServiceCenter.API/Controllers/InventoryController.cs
+++ ServiceCenter.API/Controllers/InventoryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ServiceCenter.Application.Contracts;
 using ServiceCenter.Application.DTOS;
+using ServiceCenter.Application.Services;
 using ServiceCenter.Core.Result;
 
 namespace ServiceCenter.API.Controllers;
@@ -11,16 +12,36 @@ public class InventoryController(IInventoryService inventoryService) : BaseContr
 	private readonly IInventoryService _inventoryService = inventoryService;
 
 	/// <summary>
-	/// action for add inventory action that take inventory dto   
+	/// Adds a new inventory to the system.
 	/// </summary>
-	/// <param name="inventoryRequestDto">inventory dto</param>
-	/// <returns>result for inventory added successfully.</returns>
+	/// <param name="InventoryRequestDto">The data transfer object containing developer details for creation.</param>
+	/// <remarks>
+	/// Access is limited to users with the "Admin" role.
+	/// </remarks>
+	/// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+
 	[HttpPost]
 	//[Authorize(Roles = "Admin")]
 	[ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
 	[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
-	public async Task<Result> AddTimeSlot(InventoryRequestDto inventoryRequestDto)
+	public async Task<Result> AddInventory(InventoryRequestDto inventoryRequestDto)
 	{
 		return await _inventoryService.AddInventoryAsync(inventoryRequestDto);
+	}
+
+	/// <summary>
+	/// get all inventories in the system.
+	/// </summary>
+	/// <remarks>
+	/// Access is limited to users with the "Admin" role.
+	/// </remarks>
+	/// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+	[HttpGet]
+	//[Authorize(Roles = "Admin")]
+	[ProducesResponseType(typeof(Result<List<TimeSlotResponseDto>>), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+	public async Task<Result<List<InventoryResponseDto>>> GetAllInventories()
+	{
+		return await _inventoryService.GetAllInventoriesAsync();
 	}
 }
