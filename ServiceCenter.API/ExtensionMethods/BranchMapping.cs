7be52c3455b2086c75bf9fe71f0ed21b9d95caf8
RadwaaAyman <radwa.ayman@totplatform.com>
2024-04-30 20:14:27 +0300

    enhance branch mapping

--- ServiceCenter.API/ExtensionMethods/BranchMapping.cs
+++ ServiceCenter.API/ExtensionMethods/BranchMapping.cs
@@ -2,18 +2,20 @@
 using ServiceCenter.Application.DTOS;
 using ServiceCenter.Domain.Entities;
 
-namespace ServiceCenter.API.ExtensionMethods
-{
-	public static class BranchMapping
-	{
-		public static void AddBranchMapping(this MappingProfiles map)
-		{
-			map.CreateMap<BranchRequestDto, Branch>()
-				.ReverseMap();
+namespace ServiceCenter.API.ExtensionMethods;
 
-			map.CreateMap<Branch, BranchResponseDto>()
-				.ForMember(dest=>dest.CenterName,src=>src.MapFrom(src=>src.Center.CenterName))
-				.ReverseMap();
-		}
+public static class BranchMapping
+{
+	public static void AddBranchMapping(this MappingProfiles map)
+	{
+		map.CreateMap<BranchRequestDto, Branch>()
+			.ReverseMap();
+
+		map.CreateMap<Branch, BranchResponseDto>()
+			.ForMember(dest=>dest.CenterName,src=>src.MapFrom(src=>src.Center.CenterName))
+		    .ForMember(dest => dest.Country, src => src.MapFrom(src => src.Address.Country))
+			.ForMember(dest => dest.PostalCode, src => src.MapFrom(src => src.Address.PostalCode))
+			.ForMember(dest => dest.City, src => src.MapFrom(src => src.Address.City))
+			.ReverseMap();
 	}
 }
