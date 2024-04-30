058cbbd84834d0c914992c79645c2a4baeed5afa
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 18:30:48 +0300

    add delete product action in product controller

--- ServiceCenter.API/Controllers/PrroductController.cs
+++ ServiceCenter.API/Controllers/PrroductController.cs
@@ -74,4 +74,20 @@ public class ProductController(IProductService productService) : BaseController
     {
         return await _productService.UpdateProductAsync(id, ProductRequestDto);
     }
+    /// <summary>
+    /// delete  Product  by id from the system.
+    /// </summary>
+    ///<param name="id">id</param>
+    /// <remarks>
+    /// Access is limited to users with the "Admin" role.
+    /// </remarks>
+    /// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+    [HttpDelete]
+    //[Authorize(Roles = "Admin")]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    public async Task<Result> DeleteProductAsycn(int id)
+    {
+        return await _productService.DeleteProductAsync(id);
+    }
 }
