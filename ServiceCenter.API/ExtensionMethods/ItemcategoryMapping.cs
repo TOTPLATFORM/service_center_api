0c7676a35dca50690c7c393f0f7c02b8f93c0060
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 13:59:13 +0300

    fix mapping

---
+++ ServiceCenter.API/ExtensionMethods/ItemcategoryMapping.cs
@@ -0,0 +1,17 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods;
+
+public static class ItemcategoryMapping
+{
+    public static void AddItemCategoryMapping(this MappingProfiles map)
+    {
+        map.CreateMap< ItemCategoryRequestDto,  ItemCategory>().ReverseMap();
+        map.CreateMap< ItemCategory,  ItemCategoryResponseDto>()
+            .ForMember(dest=>dest.InventoryName,src=>src.MapFrom(src=>src.Inventory.InventoryName))
+            .ReverseMap();
+        map.CreateMap<ItemCategory, ItemCategoryResponseDto>().ReverseMap();
+    }
+}
---
+++ ServiceCenter.API/ExtensionMethods/OrderMapping.cs
@@ -0,0 +1,14 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods;
+
+public static class OrderMapping
+{
+    public static void AddOrderMapping(this MappingProfiles map)
+    {
+        map.CreateMap<OrderRequestDto, Order>().ReverseMap();
+        map.CreateMap<Order, OrderResponseDto>().ReverseMap();
+    }
+}
