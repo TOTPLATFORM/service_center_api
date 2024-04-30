b7b819e8f74386838b05bc36f12bb965b7fcb84e
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 14:16:56 +0300

    add item configuration

---
+++ ServiceCenter.Infrastructure/Configuration/ItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.Property(T => T.ItemName)
+                .IsRequired();
+        builder.Property(T => T.ItemDescription)
+               .IsRequired();
+        builder.Property(T => T.ItemPrice)
+            .IsRequired();
+        builder.Property(T => T.ItemStock)
+            .IsRequired();
+        builder.Property(T => T.CategoryId)
+            .IsRequired();
+    }
+}
