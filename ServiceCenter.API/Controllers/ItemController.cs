702f99fe02fb6645d6bfe8e48c3c2795686082a2
MariamAbdeen <mariamabdeen299@gmail.com>
2024-04-30 10:40:58 +0300

    fix item controller

--- ServiceCenter.API/Controllers/ItemController.cs
+++ ServiceCenter.API/Controllers/ItemController.cs
@@ -5,80 +5,81 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using ServiceCenter.Application.Services;
 
 namespace ServiceCenter.API.Controllers;
 
-public class ItemController(IItemService itemService) : BaseController
+public class ItemController(IItemService itemService) : BaseController { 
+ private readonly IItemService _ItemService = itemService;
+
+/// <summary>
+/// action for add item action that take  Item dto   
+/// </summary>
+/// <param name="itemDto">item dto</param>
+/// <returns>result for item added successfully.</returns>
+[HttpPost]
+//[Authorize(Roles = "Admin")]
+[ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+public async Task<Result> AddItem(ItemRequestDto itemDto)
 {
-    private readonly IItemService _itemService = itemService;
-
-    /// <summary>
-    /// action for get all items.
-    /// </summary>
-    /// <returns>result of list from item response dtos</returns>
-    [HttpGet]
-    [Authorize(Roles = "Admin , Employee")]
-    [ProducesResponseType(typeof(Result<List<ItemResponseDto>>), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
-    public async Task<Result<List<ItemResponseDto>>> GetAllItemsAsync()
-    {
-        return await _itemService.GetAllItemsAsync();
-    }
-
-    /// <summary>
-    /// action for get an item by id.
-    /// </summary>
-    /// <param name="id">id for item </param>
-    /// <returns>a result of item response dto</returns>
-    [HttpGet("{id}")]
-    [Authorize(Roles = "Admin , Employee")]
-    [ProducesResponseType(typeof(Result<ItemResponseDto>), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
-    public async Task<Result<ItemResponseDto>> GetItemByIdAsync(int id)
-    {
-        return await _itemService.GetItemByIdAsync(id);
-    }
-
-    /// <summary>
-    /// action for add new item that take item request dto.
-    /// </summary>
-    /// <param name="itemDto"> item request dto </param>
-    /// <returns>result of an added item successfully</returns>
-    [HttpPost]
-    [Authorize(Roles = "Admin , Employee")]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
-    public async Task<Result> AddItemAsync(ItemRequestDto itemDto)
-    {
-        return await _itemService.AddItemAsync(itemDto);
-    }
-
-    /// <summary>
-    /// action for update an existing item that take item request dto and item id.
-    /// </summary>
-    /// <param name="itemDto">item request dto</param>
-    /// <param name="id">id of the item to update</param>
-    /// <returns>result of item response dto after updated</returns>
-    [HttpPut("{id}")]
-    [Authorize(Roles = "Admin , Employee")]
-    [ProducesResponseType(typeof(Result<ItemResponseDto>), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
-    public async Task<Result<ItemResponseDto>> UpdateItemAsync(ItemRequestDto itemDto, int id)
-    {
-        return await _itemService.UpdateItemAsync(id,itemDto);
-    }
-
-    /// <summary>
-    /// action for delete an item that take item id.
-    /// </summary>
-    /// <param name="id">id of the item</param>
-    /// <returns>result of an remove item successfully</returns>
-    [HttpDelete("{id}")]
-    [Authorize(Roles = "Admin , Employee")]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
-    public async Task<Result> DeleteItemAsync(int id)
-    {
-        return await _itemService.DeleteItemAsync(id);
-    }
+    return await _ItemService.AddItemAsync(itemDto);
 }
+
+[HttpGet]
+//[Authorize(Roles = "Admin")]
+[ProducesResponseType(typeof(Result<List<ItemResponseDto>>), StatusCodes.Status200OK)]
+[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+public async Task<Result<List<ItemResponseDto>>> GetAllItems()
+{
+    return await _ItemService.GetAllItemAsync();
+}
+/// <summary>
+/// action for get item by id that take item id.  
+/// </summary>
+/// <returns>result of item response dto</returns>
+[HttpGet("{id}")]
+//[Authorize(Roles = "Admin")]
+[ProducesResponseType(typeof(Result<ItemResponseDto>), StatusCodes.Status200OK)]
+[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+public async Task<Result<ItemResponseDto>> GetItemById(int id)
+{
+    return await _ItemService.GetItemByIdAsync(id);
+}
+[HttpPut("{id}")]
+//[Authorize(Roles = "Admin")]
+[ProducesResponseType(typeof(Result<ItemResponseDto>), StatusCodes.Status200OK)]
+[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+public async Task<Result<ItemResponseDto>> UpdateItem(int id, ItemRequestDto itemDto)
+{
+    return await _ItemService.UpdateItemAsync(id, itemDto);
+}
+/// <summary>
+///  action for remove item that take item id   
+/// </summary>
+/// <param name="id">item id</param>
+/// <returns>result of item removed successfully </returns>
+[HttpDelete]
+//  [Authorize(Roles = "Admin")]
+[ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+public async Task<Result> DeleteItemAsycn(int id)
+{
+    return await _ItemService.DeleteItemAsync(id);
+}
+
+/// <summary>
+/// function to search by Item name  that take  Item name
+/// </summary>
+/// <param name="text">Item name</param>
+/// <returns>Item response dto </returns>
+[HttpGet("search")]
+//[Authorize(Roles = "Admin")]
+[ProducesResponseType(typeof(Result<ItemResponseDto>), StatusCodes.Status200OK)]
+[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+public async Task<Result<List<ItemResponseDto>>> SearchItemByTextAsync(string text)
+{
+    return await _ItemService.SearchItemByTextAsync(text);
+}
+
+    }
