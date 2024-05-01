a96c89feb6bed071dd12e9a4c66a20286a2bdee3
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-01 12:49:02 +0300

    add employee mapping

--- ServiceCenter.API/ExtensionMethods/EmployeeMapping.cs
+++ ServiceCenter.API/ExtensionMethods/EmployeeMapping.cs
@@ -9,6 +9,10 @@ public static class EmployeeMapping
 	public static void AddEmployeeMapping(this MappingProfiles map)
 	{
 		map.CreateMap<EmployeeRequestDto, Employee>()
+			.ForMember(dest => dest.FirstName, src => src.MapFrom(src => src.EmployeeFirstName))
+			.ForMember(dest => dest.LastName, src => src.MapFrom(src => src.EmployeeLastName))
+			.ForMember(dest=>dest.PhoneNumber,src=>src.MapFrom(src => src.EmployeePhoneNumber))	
+			.ForMember(dest=>dest.Email,src=>src.MapFrom(src=>src.EmployeeEmail))
 			.ReverseMap();
 
 		map.CreateMap<Employee, EmployeeResponseDto>()
