19ba79833f8734ba25215e29dc0d5fa55fa3e91d
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 13:53:41 +0300

    add add service function in service service

---
+++ ServiceCenter.Application/Contracts/IServiceService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface IServiceService : IApplicationService, IScopedService
+{
+    /// <summary>
+    /// function to add  Service  that take  ServiceDto   
+    /// </summary>
+    /// <param name="ServiceRequestDto">Service  request dto</param>
+    /// <returns> Service  added successfully </returns>
+    public Task<Result> AddServiceAsync(ServiceRequestDto ServiceRequestDto);
+}
---
+++ ServiceCenter.Application/Services/ServiceService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services;
+
+public class ServiceService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<ServiceService> logger, IUserContextService userContext) : IServiceService
+{
+    private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<ServiceService> _logger = logger;
+    private readonly IUserContextService _userContext = userContext;
+
+    ///<inheritdoc/>
+    public async Task<Result> AddServiceAsync(ServiceRequestDto ServiceRequestDto)
+    {
+        var result = _mapper.Map<Service>(ServiceRequestDto);
+        if (result is null)
+        {
+            _logger.LogError("Failed to map ServiceRequestDto to Service. ServiceRequestDto: {@ServiceRequestDto}", ServiceRequestDto);
+            return Result.Invalid(new List<ValidationError>
+    {
+        new ValidationError
+        {
+            ErrorMessage = "Validation Errror"
+        }
+    });
+        }
+        result.CreatedBy = _userContext.Email;
+
+        _dbContext.Services.Add(result);
+
+        await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Service added successfully to the database");
+        return Result.SuccessWithMessage("Service added successfully");
+    }
+}
