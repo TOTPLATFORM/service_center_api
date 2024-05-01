bd1184ef579825929ee0ad9a6a09a4939ea04b33
Islam <104109698+IslamYasser17@users.noreply.github.com>
2024-05-01 16:15:34 +0300

    add get all contracts action in contract controller

--- ServiceCenter.API/Controllers/ContractController.cs
+++ ServiceCenter.API/Controllers/ContractController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServiceCenter.Application.Contracts;
 using ServiceCenter.Application.DTOS;
+using ServiceCenter.Application.Services;
 using ServiceCenter.Core.Result;
 
 namespace ServiceCenter.API.Controllers;
@@ -25,6 +26,23 @@ public class ContractController(IContractService ContractService) : BaseControll
     public async Task<Result> AddContract(ContractRequestDto ContractDto)
     {
         return await _ContractService.AddContractAsync(ContractDto);
+    }
+
+
+    /// <summary>
+    /// get all Contract in the system.
+    /// </summary>
+    /// <remarks>
+    /// Access is limited to users with the "Admin" role.
+    /// </remarks>
+    /// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+    [HttpGet]
+    //[Authorize(Roles = "Admin")]
+    [ProducesResponseType(typeof(Result<List<ContractResponseDto>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    public async Task<Result<List<ContractResponseDto>>> GetAllContract()
+    {
+        return await _ContractService.GetAllContractAsync();
     }
 
 }
