d25d29eefaddbed1249406e66e2c155f33aed362
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 11:39:07 +0300

    add service package configurations

---
+++ ServiceCenter.Infrastructure/Configuration/ServicePackageConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class ServicePackageConfiguration : IEntityTypeConfiguration<ServicePackage>
+{
+    public void Configure(EntityTypeBuilder<ServicePackage> builder)
+    {
+        builder.Property(T => T.PackageName)
+            .IsRequired();
+        builder.Property(T => T.PackageDescription)
+           .IsRequired();
+        builder.Property(T => T.PackagePrice)
+          .IsRequired();
+    }
+}
