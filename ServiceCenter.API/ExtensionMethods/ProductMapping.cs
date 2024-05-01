49897bc924a90457ae41c8c829209cf84e23bc4e
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 18:16:44 +0300

    enhance mapping for product

--- ServiceCenter.API/ExtensionMethods/ProductMapping.cs
+++ ServiceCenter.API/ExtensionMethods/ProductMapping.cs
@@ -9,6 +9,9 @@ public static class ProductMapping
     public static void AddProductMapping(this MappingProfiles map)
     {
         map.CreateMap<ProductRequestDto, Product >().ReverseMap();
-        map.CreateMap<Product, ProductResponseDto>().ReverseMap();
+        map.CreateMap<Product, ProductResponseDto>()
+            .ForMember(dest => dest.CategoryName, src => src.MapFrom(src => src.ProductCategory.CategoryName))
+            .ForMember(dest => dest.ProductBrandName, src => src.MapFrom(src => src.ProductBrand.BrandName))
+            .ReverseMap();
     }
 }
