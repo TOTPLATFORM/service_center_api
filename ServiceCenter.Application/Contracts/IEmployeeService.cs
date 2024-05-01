69e6e33253ef8b016c8f02a0bfac6dfda76897c6
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-01 12:44:28 +0300

    add  add employee function in employee service

--- ServiceCenter.Application/Contracts/IEmployeeService.cs
+++ ServiceCenter.Application/Contracts/IEmployeeService.cs
@@ -16,4 +16,47 @@ public interface IEmployeeService : IApplicationService, IScopedService
 	/// <param name="employeeRequestDto">employee request dto</param>
 	/// <returns>Employee added successfully </returns>
 	public Task<Result> AddEmployeeAsync(EmployeeRequestDto employeeRequestDto);
+
+	/// <summary>
+	/// function to get all employees 
+	/// </summary>
+	/// <returns>list all employeeResponseDto </returns>
+	public Task<Result<List<EmployeeResponseDto>>> GetAllEmployeesAsync();
+
+	/// <summary>
+	/// function to get employee by id that take  employee id
+	/// </summary>
+	/// <param name="id">employee id</param>
+	/// <returns>employee response dto</returns>
+	public Task<Result<EmployeeResponseDto>> GetEmployeeByIdAsync(int id);
+
+	/// <summary>
+	/// function to update employee that take EmployeeRequestDto   
+	/// </summary>
+	/// <param name="id">employee id</param>
+	/// <param name="employeeRequestDto">employee dto</param>
+	/// <returns>Updated Employee </returns>
+	public Task<Result<EmployeeResponseDto>> UpdateEmployeeAsync(int id, EmployeeRequestDto employeeRequestDto);
+
+
+	/// <summary>
+	/// function to search employee by text  that take text   
+	/// </summary>
+	/// <param name="text">text</param>
+	/// <returns>all employeees that contain this text </returns>
+	public Task<Result<List<EmployeeResponseDto>>> SearchEmployeeByTextAsync(string text);
+
+	/// <summary>
+	/// function to search employee by relation  that take text   
+	/// </summary>
+	/// <param name="text">text</param>
+	/// <returns>all employeees that contain this relation </returns>
+	public Task<Result<List<EmployeeResponseDto>>> GetAllItemsCategoryForSpecificInventoryAsync(string text);
+
+	/// <summary>
+	/// function to delete Employee that take EmployeeDto   
+	/// </summary>
+	/// <param name="id">departmnet id</param>
+	/// <returns>Employee removed successfully </returns>
+	public Task<Result> DeleteEmployeeAsync(int id);
 }
--- ServiceCenter.Application/Services/EmployeeService.cs
+++ ServiceCenter.Application/Services/EmployeeService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ServiceCenter.Application.Contracts;
@@ -20,7 +21,7 @@ public class EmployeeService(ServiceCenterBaseDbContext dbContext, IMapper mappe
 	private readonly IMapper _mapper = mapper;
 	private readonly ILogger<IEmployeeService> _logger = logger;
 	private readonly IUserContextService _userContext = userContext;
-	private readonly IAuthService _authService;
+	private readonly IAuthService _authService = authService;
 
 
 	///<inheritdoc/>
@@ -48,5 +49,104 @@ public class EmployeeService(ServiceCenterBaseDbContext dbContext, IMapper mappe
 		_logger.LogInformation("Employee added successfully in the database");
 
 		return Result.SuccessWithMessage("Employee added successfully");
+	}
+
+	public async Task<Result<List<EmployeeResponseDto>>> GetAllEmployeesAsync()
+	{
+		var employees = await _dbContext.Users.OfType<Employee>()
+				  .ProjectTo<EmployeeResponseDto>(_mapper.ConfigurationProvider)
+				  .ToListAsync();
+		_logger.LogInformation("Fetching all employee. Total count: {employee}.", employees.Count);
+		return Result.Success(employees);
+	}
+
+	///<inheritdoc/>
+	public async Task<Result> DeleteEmployeeAsync(string id)
+	{
+		var employee = await _dbContext.Employees.FindAsync(id);
+
+		if (employee is null)
+		{
+			_logger.LogWarning("employee Invaild Id ,Id {employeeId}", id);
+			return Result.NotFound(["employee Invaild Id"]);
+		}
+
+		_dbContext.Employees.Remove(employee);
+		await _dbContext.SaveChangesAsync();
+		_logger.LogInformation("employee remove successfully in the database");
+		return Result.SuccessWithMessage("employee remove successfully ");
+	}
+
+	///<inheritdoc/>
+	public async Task<Result<EmployeeResponseDto>> GetEmployeeByIdAsync(string Id)
+	{
+		var employee = await _dbContext.Employees
+			.ProjectTo<EmployeeResponseDto>(_mapper.ConfigurationProvider)
+			.FirstOrDefaultAsync(d => d.Id == Id);
+		if (employee is null)
+		{
+			_logger.LogWarning("employee Id not found,Id {employeeId}", Id);
+			return Result.NotFound(["employee not found"]);
+		}
+		_logger.LogInformation("Fetching employee");
+		return Result.Success(employee);
+	}
+
+	///<inheritdoc/>
+	public async Task<Result<EmployeeResponseDto>> UpdateEmployeeAsync(string id, EmployeeRequestDto employeeRequestDto)
+	{
+		var employee = await _dbContext.Employees.FindAsync(id);
+
+		if (employee is null)
+		{
+			_logger.LogWarning("employee Id not found,Id {employeeId}", id);
+			return Result.NotFound(["employee not found"]);
+		}
+
+		_mapper.Map(employeeRequestDto, employee);
+
+		await _dbContext.SaveChangesAsync();
+
+		var employeeResponse = _mapper.Map<EmployeeResponseDto>(employee);
+		if (employeeResponse is null)
+		{
+			_logger.LogError("Failed to map employeeRequestDto to employeeResponseDto. employeeRequestDto: {@employeeRequestDto}", employeeRequestDto);
+			return Result.Invalid(new List<ValidationError>
+				{
+					new ValidationError
+					{
+						ErrorMessage = "Validation Errror"
+					}
+				});
+		}
+
+		_logger.LogInformation("Updated employee , Id {Id}", id);
+
+		return Result.Success(employeeResponse);
+	}
+
+	public Task<Result<EmployeeResponseDto>> GetEmployeeByIdAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Result<EmployeeResponseDto>> UpdateEmployeeAsync(int id, EmployeeRequestDto employeeRequestDto)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Result<List<EmployeeResponseDto>>> SearchEmployeeByTextAsync(string text)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Result<List<EmployeeResponseDto>>> GetAllItemsCategoryForSpecificInventoryAsync(string text)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Result> DeleteEmployeeAsync(int id)
+	{
+		throw new NotImplementedException();
 	}
 }
