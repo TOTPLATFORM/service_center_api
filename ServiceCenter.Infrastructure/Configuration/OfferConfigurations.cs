a1d7f02978e2ea859d42072dd40a9db6beb6bfc8
Islam <104109698+IslamYasser17@users.noreply.github.com>
2024-05-01 18:05:59 +0300

    add offer configurations

---
+++ ServiceCenter.Infrastructure/Configuration/OfferConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceCenter.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class OfferConfigurations : IEntityTypeConfiguration<Offer>
+{
+    public void Configure(EntityTypeBuilder<Offer> builder)
+    {
+        builder.Property(p => p.OfferName)
+               .IsRequired()
+              .HasMaxLength(50);
+        builder.Property(p => p.OfferDescription)
+              .HasMaxLength(150)
+               .IsRequired();
+        builder.Property(p => p.StartDate)
+       .IsRequired();
+        builder.Property(p => p.EndDate).IsRequired();
+        builder.Property(p => p.Discount).IsRequired();
+        
+
+    }
+}
