7c87c445b09be0c96e25cdcd59d0592370f04a2d
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 11:17:27 +0300

    add service category configurations

---
+++ ServiceCenter.Infrastructure/Configuration/ServiceCategoryConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class ServiceCategoryConfigurations : IEntityTypeConfiguration<ServiceCategory>
+{
+    public void Configure(EntityTypeBuilder<ServiceCategory> builder)
+    {
+        builder.Property(T => T.ServiceCategoryName)
+            .IsRequired();
+        builder.Property(T=>T.ServiceCategoryDescription)
+            .IsRequired();
+
+    }
+}
