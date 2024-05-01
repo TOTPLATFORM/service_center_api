afcc65bcdf0f7f634c063e04547669bc94ceeba0
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 06:54:35 +0300

    add get room by id action in room controller

--- ServiceCenter.API/Controllers/RoomController.cs
+++ ServiceCenter.API/Controllers/RoomController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ServiceCenter.Application.Contracts;
 using ServiceCenter.Application.DTOS;
+using ServiceCenter.Application.Services;
 using ServiceCenter.Core.Result;
 
 namespace ServiceCenter.API.Controllers;
@@ -25,5 +26,36 @@ public class RoomController(IRoomService RoomService) : BaseController
     public async Task<Result> AddRoom(RoomRequestDto RoomDto)
     {
         return await _RoomService.AddRoomAsync(RoomDto);
+    }
+    /// <summary>
+    /// get all Room categories in the system.
+    /// </summary>
+    /// <remarks>
+    /// Access is limited to users with the "Admin" role.
+    /// </remarks>
+    /// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+    [HttpGet]
+    //[Authorize(Roles = "Admin")]
+    [ProducesResponseType(typeof(Result<List<RoomResponseDto>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    public async Task<Result<List<RoomResponseDto>>> GetAllRoom()
+    {
+        return await _RoomService.GetAllRoomAsync();
+    }
+    /// <summary>
+    /// get Room by id in the system.
+    /// </summary>
+    ///<param name="id">id of Room.</param>
+    /// <remarks>
+    /// Access is limited to users with the "Admin" role.
+    /// </remarks>
+    /// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+    [HttpGet("{id}")]
+    //[Authorize(Roles = "Admin")]
+    [ProducesResponseType(typeof(Result<RoomResponseDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+    public async Task<Result<RoomResponseDto>> GetRoomById(int id)
+    {
+        return await _RoomService.GetRoomByIdAsync(id);
     }
 }
