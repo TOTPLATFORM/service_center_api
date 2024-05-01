f0afb0c35da75c4ae987bd4fb4294948fc1ea4dc
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 06:43:28 +0300

    add add room function in room service

---
+++ ServiceCenter.Application/Contracts/IRoomService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface IRoomService : IApplicationService, IScopedService
+{
+    /// <summary>
+    /// function to add  Room  that take  RoomDto   
+    /// </summary>
+    /// <param name="RoomRequestDto">Room  request dto</param>
+    /// <returns> Room  added successfully </returns>
+    public Task<Result> AddRoomAsync(RoomRequestDto RoomRequestDto);
+}
---
+++ ServiceCenter.Application/Services/RoomService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services;
+
+public class RoomService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<RoomService> logger, IUserContextService userContext) : IRoomService
+{
+    private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<RoomService> _logger = logger;
+    private readonly IUserContextService _userContext = userContext;
+
+    ///<inheritdoc/>
+    public async Task<Result> AddRoomAsync(RoomRequestDto RoomRequestDto)
+    {
+        var result = _mapper.Map<Room>(RoomRequestDto);
+        if (result is null)
+        {
+            _logger.LogError("Failed to map RoomRequestDto to Room. RoomRequestDto: {@RoomRequestDto}", RoomRequestDto);
+            return Result.Invalid(new List<ValidationError>
+    {
+        new ValidationError
+        {
+            ErrorMessage = "Validation Errror"
+        }
+    });
+        }
+        result.CreatedBy = _userContext.Email;
+
+        _dbContext.Rooms.Add(result);
+
+        await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Room added successfully to the database");
+        return Result.SuccessWithMessage("Room added successfully");
+    }
+}
