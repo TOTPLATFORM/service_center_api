62c743e504ffb70ea5ce65eea91f245ab8011644
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 13:32:30 +0300

    add service configurations

---
+++ ServiceCenter.Infrastructure/Configuration/serviceConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public  class serviceConfigurations : IEntityTypeConfiguration<Service>
+{
+    public void Configure(EntityTypeBuilder<Service> builder)
+    {
+        builder.Property(T => T.ServiceName)
+            .IsRequired();
+        builder.Property(T => T.ServiceDescription)
+           .IsRequired();
+        builder.Property(T => T.ServicePrice)
+          .IsRequired();
+        builder.Property(T => T.Avaliable)
+               .IsRequired();
+    }
+}
