0445225ca6bef3fbc43f8c1a01cccf07fce820fa
Islam <104109698+IslamYasser17@users.noreply.github.com>
2024-05-01 12:25:05 +0300

    add add compaint function in complaint service

---
+++ ServiceCenter.Application/Contracts/ICompliantService.cs
@@ -0,0 +1,20 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface IComplaintService : IApplicationService, IScopedService
+{
+    /// <summary>
+    /// function to add  Complaint  that take  ComplaintDto   
+    /// </summary>
+    /// <param name="ComplaintRequestDto">Complaint  request dto</param>
+    /// <returns> Complaint  added successfully </returns>
+    public Task<Result> AddComplaintAsync(ComplaintRequestDto ComplaintRequestDto);
+
+}
---
+++ ServiceCenter.Application/Services/CompliantService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ComplaintService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<ComplaintService> logger, IUserContextService userContext) : IComplaintService
+{
+    private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<ComplaintService> _logger = logger;
+    private readonly IUserContextService _userContext = userContext;
+
+    ///<inheritdoc/>
+    public async Task<Result> AddComplaintAsync(ComplaintRequestDto ComplaintRequestDto)
+    {
+        var result = _mapper.Map<Complaint>(ComplaintRequestDto);
+        if (result is null)
+        {
+            _logger.LogError("Failed to map ComplaintRequestDto to Complaint. ComplaintRequestDto: {@ComplaintRequestDto}", ComplaintRequestDto);
+            return Result.Invalid(new List<ValidationError>
+    {
+        new ValidationError
+        {
+            ErrorMessage = "Validation Errror"
+        }
+    });
+        }
+        result.CreatedBy = _userContext.Email;
+
+        _dbContext.Complaints.Add(result);
+
+        await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Complaint added successfully to the database");
+        return Result.SuccessWithMessage("Complaint added successfully");
+    }
+
+}
+
