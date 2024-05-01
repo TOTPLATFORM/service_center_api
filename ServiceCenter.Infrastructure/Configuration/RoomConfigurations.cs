b5f18f4e1f56e42c3202d469b81c42ece18d76b1
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 06:30:25 +0300

    add room configurations

---
+++ ServiceCenter.Infrastructure/Configuration/RoomConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class RoomConfigurations : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.Property(T => T.RoomNumber)
+               .IsRequired();
+        builder.Property(T => T.Availability)
+              .IsRequired();
+
+    }
+}
