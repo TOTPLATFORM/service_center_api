a5dfe125f5d6e2392bcce40e6d2249097eff6510
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 12:30:33 +0300

    add add service package function in service package service

---
+++ ServiceCenter.Application/Contracts/IServicePackageService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface IServicePackageService : IApplicationService, IScopedService
+{
+    /// <summary>
+    /// function to add  ServicePackage  that take  ServicePackageDto   
+    /// </summary>
+    /// <param name="ServicePackageRequestDto">ServicePackage  request dto</param>
+    /// <returns> ServicePackage  added successfully </returns>
+    public Task<Result> AddServicePackageAsync(ServicePackageRequestDto ServicePackageRequestDto);
+}
---
+++ ServiceCenter.Application/Services/ServicePackageservice.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services;
+
+public class ServicePackageService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<ServicePackageService> logger, IUserContextService userContext) : IServicePackageService
+{
+    private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<ServicePackageService> _logger = logger;
+    private readonly IUserContextService _userContext = userContext;
+
+    ///<inheritdoc/>
+    public async Task<Result> AddServicePackageAsync(ServicePackageRequestDto ServicePackageRequestDto)
+    {
+        var result = _mapper.Map<ServicePackage>(ServicePackageRequestDto);
+        if (result is null)
+        {
+            _logger.LogError("Failed to map ServicePackageRequestDto to ServicePackage. ServicePackageRequestDto: {@ServicePackageRequestDto}", ServicePackageRequestDto);
+            return Result.Invalid(new List<ValidationError>
+{
+    new ValidationError
+    {
+        ErrorMessage = "Validation Errror"
+    }
+});
+        }
+        result.CreatedBy = _userContext.Email;
+
+        _dbContext.ServicePackages.Add(result);
+
+        await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("ServicePackage added successfully to the database");
+        return Result.SuccessWithMessage("ServicePackage added successfully");
+    }
+}
