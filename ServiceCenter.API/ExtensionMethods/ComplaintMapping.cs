7e48343c75b9fef926739a2bb2f6d3095c003b9a
Islam <104109698+IslamYasser17@users.noreply.github.com>
2024-05-01 11:38:39 +0300

    add mapping for compliant

---
+++ ServiceCenter.API/ExtensionMethods/ComplaintMapping.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods
+{
+    public static class ComplaintMapping
+    {
+        public static void AddCompliantMapping(this MappingProfiles map)
+        {
+            map.CreateMap<ComplaintRequestDto, Complaint>().ReverseMap();
+            map.CreateMap<Complaint,ComplaintResponseDto>()
+                .ForMember(dest=>dest.CustomerName , src => src.MapFrom(src=>src.Customer.FirstName))
+                .ReverseMap();
+
+        
+        }
+    }
+}
