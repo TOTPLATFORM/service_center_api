609183d504a6c93bca8a03d6fd4e0b4fb82be3a1
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 07:36:07 +0300

    add feedback Configurations

---
+++ ServiceCenter.Infrastructure/Configuration/Feedbackconfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class Feedbackconfigurations : IEntityTypeConfiguration<Feedback>
+{
+    public void Configure(EntityTypeBuilder<Feedback> builder)
+    {
+        builder.Property(T => T.FeedbackDescription)
+             .IsRequired();
+        builder.Property(T => T.FeedbackDate)
+              .IsRequired();
+        
+    }
+}
