fa3d55b88018d07a183f69ce5f14cd68bd4fcfb1
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-01 18:25:46 +0300

    add appointment test

---
+++ ServiceCenter.Test/TestServices/AppointmentServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Test.TestServices;
+[TestCaseOrderer(
+ordererTypeName: "ServiceCenter.Test.TestPriority.PriorityOrderer",
+ordererAssemblyName: "ServiceCenter.Test")]
+
+internal class AppointmentServiceTest
+{
+}
---
+++ ServiceCenter.Test/TestSetup/Data/AppointmentTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Test.TestSetup.Data;
+
+internal class AppointmentTest
+{
+}
