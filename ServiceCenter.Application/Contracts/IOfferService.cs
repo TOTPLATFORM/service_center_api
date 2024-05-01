851da4e94311ef922c27752e7492b49ae0e9c5ac
Islam <104109698+IslamYasser17@users.noreply.github.com>
2024-05-01 18:08:05 +0300

    add offer function in offer service

---
+++ ServiceCenter.Application/Contracts/IOfferService.cs
@@ -0,0 +1,44 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface IOfferService : IApplicationService, IScopedService
+{
+    /// <summary>
+    /// function to add  Offer  that take  OfferDto   
+    /// </summary>
+    /// <param name="OfferRequestDto">Offer  request dto</param>
+    /// <returns> Offer  added successfully </returns>
+    public Task<Result> AddOfferAsync(OfferRequestDto OfferRequestDto);
+    /// <summary>
+    /// function to get all Offer  
+    /// </summary>
+    /// <returns>list all Offer  response dto </returns>
+    public Task<Result<List<OfferResponseDto>>> GetAllOfferAsync();
+    /// <summary>
+    /// function to get  Offer  by id that take   Offer id
+    /// </summary>
+    /// <param name="id"> Offer  id</param>
+    /// <returns> Offer  response dto</returns>
+    public Task<Result<OfferResponseDto>> GetOfferByIdAsync(int id);
+    /// <summary>
+    /// function to update Offer  that take OfferRequestDto   
+    /// </summary>
+    /// <param name="id">Offer id</param>
+    /// <param name="OfferRequestDto">Offer dto</param>
+    /// <returns>Updated Offer </returns>
+    public Task<Result<OfferResponseDto>> UpdateOfferAsync(int id, OfferRequestDto OfferRequestDto);
+    /// <summary>
+    /// function to delete Offer  that take Offer  id   
+    /// </summary>
+    /// <param name="id">Offer  id</param>
+    /// <returns>Offer  removed successfully </returns>
+    public Task<Result> DeleteOfferAsync(int id);
+
+}
---
+++ ServiceCenter.Application/Services/OfferService.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services;
+
+public class OfferService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<OfferService> logger, IUserContextService userContext) : IOfferService
+{
+    private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<OfferService> _logger = logger;
+    private readonly IUserContextService _userContext = userContext;
+
+    ///<inheritdoc/>
+    public async Task<Result> AddOfferAsync(OfferRequestDto OfferRequestDto)
+    {
+        var result = _mapper.Map<Offer>(OfferRequestDto);
+        if (result is null)
+        {
+            _logger.LogError("Failed to map OfferRequestDto to Offer. OfferRequestDto: {@OfferRequestDto}", OfferRequestDto);
+            return Result.Invalid(new List<ValidationError>
+    {
+        new ValidationError
+        {
+            ErrorMessage = "Validation Errror"
+        }
+    });
+        }
+        result.CreatedBy = _userContext.Email;
+
+        _dbContext.Offers.Add(result);
+
+        await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Offer added successfully to the database");
+        return Result.SuccessWithMessage("Offer added successfully");
+    }
+    ///<inheritdoc/>
+    public async Task<Result<List<OfferResponseDto>>> GetAllOfferAsync()
+    {
+        var result = await _dbContext.Offers
+             .ProjectTo<OfferResponseDto>(_mapper.ConfigurationProvider)
+             .ToListAsync();
+
+        _logger.LogInformation("Fetching all  Offer. Total count: { Offer}.", result.Count);
+
+        return Result.Success(result);
+    }
+    ///<inheritdoc/>
+    public async Task<Result<OfferResponseDto>> GetOfferByIdAsync(int id)
+    {
+        var result = await _dbContext.Offers
+            .ProjectTo<OfferResponseDto>(_mapper.ConfigurationProvider)
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (result is null)
+        {
+            _logger.LogWarning("Offer Id not found,Id {OfferId}", id);
+
+            return Result.NotFound(["Offer not found"]);
+        }
+
+        _logger.LogInformation("Fetching Offer");
+
+        return Result.Success(result);
+    }
+    ///<inheritdoc/>
+    public async Task<Result<OfferResponseDto>> UpdateOfferAsync(int id, OfferRequestDto OfferRequestDto)
+    {
+        var result = await _dbContext.Offers.FindAsync(id);
+
+        if (result is null)
+        {
+            _logger.LogWarning("Offer Id not found,Id {OfferId}", id);
+            return Result.NotFound(["Offer not found"]);
+        }
+
+        result.ModifiedBy = _userContext.Email;
+
+        _mapper.Map(OfferRequestDto, result);
+
+        await _dbContext.SaveChangesAsync();
+
+        var OfferResponse = _mapper.Map<OfferResponseDto>(result);
+        if (OfferResponse is null)
+        {
+            _logger.LogError("Failed to map OfferRequestDto to OfferResponseDto. OfferRequestDto: {@OfferRequestDto}", OfferResponse);
+
+            return Result.Invalid(new List<ValidationError>
+        {
+                new ValidationError
+                {
+                    ErrorMessage = "Validation Errror"
+                }
+        });
+        }
+
+        _logger.LogInformation("Updated Offer , Id {Id}", id);
+
+        return Result.Success(OfferResponse);
+    }
+    ///<inheritdoc/>
+    public async Task<Result> DeleteOfferAsync(int id)
+    {
+        var Offer = await _dbContext.Offers.FindAsync(id);
+
+        if (Offer is null)
+        {
+            _logger.LogWarning("Offer Invaild Id ,Id {OfferId}", id);
+            return Result.NotFound(["Offer Invaild Id"]);
+        }
+
+        _dbContext.Offers.Remove(Offer);
+        await _dbContext.SaveChangesAsync();
+        _logger.LogInformation("Offer removed successfully in the database");
+        return Result.SuccessWithMessage("Offer removed successfully");
+    }
+
+
+
+}
