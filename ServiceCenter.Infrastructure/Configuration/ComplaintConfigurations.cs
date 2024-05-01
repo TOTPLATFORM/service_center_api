ff94cec0ef768f2d893bbe33d797c21e891264e5
Islam <104109698+IslamYasser17@users.noreply.github.com>
2024-05-01 11:21:59 +0300

    add compliant configurations

---
+++ ServiceCenter.Infrastructure/Configuration/ComplaintConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ComplaintConfigurations : IEntityTypeConfiguration<Complaint>
+{
+    public void Configure(EntityTypeBuilder<Complaint> builder)
+    {
+        builder.Property(T => T.CreatedDate)
+               .IsRequired();
+        builder.Property(T => T.CreatedBy)
+              .IsRequired();
+        builder.Property(T => T.CustomerId)
+              .IsRequired();
+        builder.Property(T => T.ComplaintStatus)
+              .IsRequired();
+        builder.Property(T => T.ComplaintDescription)
+              .IsRequired();
+
+    }
+}
