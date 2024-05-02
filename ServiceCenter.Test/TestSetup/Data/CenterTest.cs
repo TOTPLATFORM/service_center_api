a12e3da7ff5788d980376e3d12e35517d2de069b
MariamAbdeen <mariamabdeen299@gmail.com>
2024-05-02 14:42:08 +0300

    add test for center service

---
+++ ServiceCenter.Test/TestSetup/Data/CenterTest.cs
@@ -0,0 +1,32 @@
+﻿using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Test.TestSetup.Data;
+
+public static class CenterTest
+{
+    public static void AddCenter(this ServiceCenterBaseDbContext context)
+    {
+        context.Centers.AddRange(
+        new Center
+        {
+            Id = 1,
+            OpeningHours = 10,
+            Specialty="center1",
+
+        },
+        new Center
+        {
+
+            Id = 2,
+            OpeningHours = 12,
+            Specialty = "center2",
+        }
+        ); 
+    }
+}
