824343070c908cb3e7ec7b5aaf96901ca937d973
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-13 18:57:49 +0300

    add mapping for contact

--- ServiceCenter.API/ExtensionMethods/ContactMapping.cs
+++ ServiceCenter.API/ExtensionMethods/ContactMapping.cs
@@ -8,5 +8,14 @@ public static class ContactMapping
 	public static void AddContactMapping(this MappingProfiles map)
 	{
 		map.CreateMap<ContactRequestDto, Contact>().ReverseMap();
+
+		map.CreateMap<Contact,ContactResponseDto>()
+			.ForMember(dest=>dest.FirstName,src=>src.MapFrom(src=>src.ContactFirstName))
+			.ForMember(dest => dest.LastName, src => src.MapFrom(src => src.ContactLastName))
+			.ForMember(dest => dest.Email, src => src.MapFrom(src => src.ContactEmail))
+			.ForMember(dest => dest.City, src => src.MapFrom(src => src.Address.City))
+			.ForMember(dest => dest.Country, src => src.MapFrom(src => src.Address.Country))
+			.ForMember(dest => dest.PostalCode, src => src.MapFrom(src => src.Address.PostalCode))
+			.ReverseMap();	
 	}
 }
