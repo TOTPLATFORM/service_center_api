9f53cde9488ea43efd860c65f997daaede1f9b7d
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-13 18:24:42 +0300

    add add contact fucntion in contact service

---
+++ ServiceCenter.Application/Contracts/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface IContactService : IApplicationService, IScopedService
+{
+	/// <summary>
+	/// function to add contact that take contactDto   
+	/// </summary>
+	/// <param name="contactRequestDto">contact request dto</param>
+	/// <returns>Branch added successfully </returns>
+	public Task<Result> AddContactAsync(ContactRequestDto contactRequestDto);
+}
---
+++ ServiceCenter.Application/Services/ContactService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services;
+
+public class ContactService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<ContactService> logger, IUserContextService userContext) : IContactService
+{
+	private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+	private readonly IMapper _mapper = mapper;
+	private readonly ILogger<ContactService> _logger = logger;
+	private readonly IUserContextService _userContext = userContext;
+
+
+	///<inheritdoc/>
+	public async Task<Result> AddContactAsync(ContactRequestDto contactRequestDto)
+	{
+		var result = _mapper.Map<Contact>(contactRequestDto);
+
+		if (result is null)
+		{
+			_logger.LogError("Failed to map ContactRequestDto to Contact. ContactRequestDto: {@ContactRequestDto}", contactRequestDto);
+
+			return Result.Invalid(new List<ValidationError>
+			{
+				new ValidationError
+				{
+					ErrorMessage = "Validation Errror"
+				}
+			});
+		}
+		result.CreatedBy = _userContext.Email;
+
+		_dbContext.Contacts.Add(result);
+
+		await _dbContext.SaveChangesAsync();
+
+		_logger.LogInformation("Contact added successfully to the database");
+
+		return Result.SuccessWithMessage("Contact added successfully");
+	}
+
+}
+
