b4efe14408b0160d6afb693b578a079e575995af
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-13 18:23:54 +0300

    add mapping for contact

---
+++ ServiceCenter.API/ExtensionMethods/ContactMapping.cs
@@ -0,0 +1,12 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods;
+public static class ContactMapping
+{
+	public static void AddContactMapping(this MappingProfiles map)
+	{
+		map.CreateMap<ContactRequestDto, Contact>().ReverseMap();
+	}
+}
