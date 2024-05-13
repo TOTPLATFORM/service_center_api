5e135101c693b12b75d4f88755aeb6c2f63e79ba
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-13 18:24:19 +0300

    add contact dto

---
+++ ServiceCenter.Application/DTOS/ContactRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using ServiceCenter.Domain.Entities;
+using ServiceCenter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS;
+
+public class ContactRequestDto
+{
+	[Required]
+	public string ContactFirstName { get; set; } = "";
+	[Required]
+	public string ContactLastName { get; set; } = "";
+	[Required]
+	[EmailAddress]
+	public string ContactEmail { get; set; } = "";
+	[Required]
+	public string Gender { get; set; } = "";
+	public Address Address { get; set; } = default;
+}
