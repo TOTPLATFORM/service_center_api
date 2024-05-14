74278461b88453771913756dd983f8337f5f5a6b
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-14 14:42:01 +0300

    add get all customers function in customer service

---
+++ ServiceCenter.Application/Contracts/ICustomerService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface ICustomerService : IApplicationService, IScopedService
+{
+
+	/// <summary>
+	/// function to get all customers 
+	/// </summary>
+	/// <returns>list all customerResponseDto </returns>
+	public Task<Result<List<CustomerResponseDto>>> GetAllCustomersAsync();
+}
