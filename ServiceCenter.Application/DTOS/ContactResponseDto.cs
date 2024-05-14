9ad5bdca848cd3fb51b917285950e3bd3594368a
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-14 14:16:42 +0300

    enhance in contact response dto

--- ServiceCenter.Application/DTOS/ContactResponseDto.cs
+++ ServiceCenter.Application/DTOS/ContactResponseDto.cs
@@ -11,7 +11,7 @@ namespace ServiceCenter.Application.DTOS;
 
 public class ContactResponseDto
 {
-    public int ContactId { get; set; }
+    public int Id { get; set; }
     public string FirstName { get; set; } = "";
 	public string LastName { get; set; } = "";
 	public string Email { get; set; } = "";
