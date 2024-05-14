7db1b070d79741998f5e42fd26224e433acf7ee8
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-14 14:42:31 +0300

    add get all customers action in customer controller

---
+++ ServiceCenter.API/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Application.Services;
+using ServiceCenter.Core.Result;
+
+namespace ServiceCenter.API.Controllers;
+
+public class CustomerController(ICustomerService customerService) : BaseController
+{
+	private readonly ICustomerService _customerService = customerService;
+	/// <summary>
+	/// get all customers in the system.
+	/// </summary>
+	/// <remarks>
+	/// Access is limited to users with the "Admin" role.
+	/// </remarks>
+	/// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+	[HttpGet]
+	//[Authorize(Roles = "Admin")]
+	[ProducesResponseType(typeof(Result<List<CustomerResponseDto>>), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+	public async Task<Result<List<CustomerResponseDto>>> GetAllCustomers()
+	{
+		return await _customerService.GetAllCustomersAsync();
+	}
+
+	/// <summary>
+	/// get all customers in the system.
+	/// </summary>
+	///<param name="id">id of customer.</param>
+	/// <remarks>
+	/// Access is limited to users with the "Admin" role.
+	/// </remarks>
+	/// <returns>A task that represents the asynchronous operation, which encapsulates the result of the addition process.</returns>
+	[HttpGet("{id}")]
+	//[Authorize(Roles = "Admin")]
+	[ProducesResponseType(typeof(Result<CustomerResponseDto>), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(Result), StatusCodes.Status400BadRequest)]
+	public async Task<Result<CustomerResponseDto>> GetCustomerById(string id)
+	{
+		return await _customerService.GetCustomerByIdAsync(id);
+	}
+}
