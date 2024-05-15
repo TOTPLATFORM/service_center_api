1aa3ee9b40f16a3fd1cfeb39f1c037862e06e791
hager3012 <hager3012>
2024-05-15 15:50:10 +0300

    add appointment request dto

--- ServiceCenter.Application/DTOS/AppointmentRequestDto.cs
+++ ServiceCenter.Application/DTOS/AppointmentRequestDto.cs
@@ -10,15 +10,7 @@ namespace ServiceCenter.Application.DTOS;
 public class AppointmentRequestDto
 {
     [Required]
-    public TimeOnly StartTime { get; set; }
-    [Required]
-    public TimeOnly EndTime { get; set; }
-    [Required]
-    public DateOnly Date { get; set; }
-    [Required]
-    public string Day { get; set; } = "";
+    public int ScheduleId { get; set; }
     [Required]
     public string CustomerId { get; set; } = "";
-    [Required]
-    public string EmployeeId { get; set; } = "";
 }
