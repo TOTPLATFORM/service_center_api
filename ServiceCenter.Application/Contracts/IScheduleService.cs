7f50b5693579036bf333938dc2bd5c819d62c799
hager3012 <hager3012>
2024-05-15 15:49:28 +0300

    enhance in i schedule service contract

--- ServiceCenter.Application/Contracts/IScheduleService.cs
+++ ServiceCenter.Application/Contracts/IScheduleService.cs
@@ -11,34 +11,48 @@ namespace ServiceCenter.Application.Contracts;
 public interface IScheduleService : IApplicationService, IScopedService
 {
     /// <summary>
-    /// function to add  Schedule  that take  ScheduleDto   
+    /// asynchronously adds a new schedule to the database.
     /// </summary>
-    /// <param name="ScheduleRequestDto">Schedule  request dto</param>
-    /// <returns> Schedule  added successfully </returns>
-    public Task<Result> AddScheduleAsync(ScheduleRequestDto ScheduleRequestDto);
+    /// <param name="scheduleRequestDto">the schedule data transfer object containing the details necessary for creation.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the schedule addition.</returns>
+    public Task<Result> AddScheduleAsync(ScheduleRequestDto scheduleRequestDto);
     /// <summary>
-    /// function to get all Schedule  
+    /// asynchronously retrieves all schedules in the system.
     /// </summary>
-    /// <returns>list all Schedule  response dto </returns>
-    public Task<Result<List<ScheduleResponseDto>>> GetAllScheduleAsync();
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result containing a list of schedule response DTOs.</returns>
+    public Task<Result<List<ScheduleResponseDto>>> GetAllSchedulesAsync();
+
     /// <summary>
-    /// function to get  Schedule  by id that take   Schedule id
+    /// asynchronously searches for schedules based on the provided text.
     /// </summary>
-    /// <param name="id"> Schedule  id</param>
-    /// <returns> Schedule  response dto</returns>
-    public Task<Result<ScheduleResponseDto>> GetScheduleByIdAsync(int id);
+    /// <param name="text">the text to search within schedule data.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result containing a list of schedule response DTOs that match the search criteria.</returns>
+    public Task<Result<List<ScheduleResponseDto>>> SearchSchedulesByTextAsync(string text);
     /// <summary>
-    /// function to update Schedule  that take ScheduleRequestDto   
+    /// asynchronously retrieves a schedule by their unique identifier.
     /// </summary>
-    /// <param name="id">Schedule id</param>
-    /// <param name="ScheduleRequestDto">Schedule dto</param>
-    /// <returns>Updated Schedule </returns>
-    public Task<Result<ScheduleResponseDto>> UpdateScheduleAsync(int id, ScheduleRequestDto ScheduleRequestDto);
+    /// <param name="id">the unique identifier of the schedule to retrieve.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result containing the schedule response DTO.</returns>
+    public Task<Result<ScheduleGetByIdResponseDto>> GetScheduleByIdAsync(int id);
     /// <summary>
-    /// function to delete Schedule  that take Schedule  id   
+    /// asynchronously updates the data of an existing schedule.
     /// </summary>
-    /// <param name="id">Schedule  id</param>
-    /// <returns>Schedule  removed successfully </returns>
+    /// <param name="id">the unique identifier of the schedule to update.</param>
+    /// <param name="scheduleRequestDto">the schedule data transfer object containing the updated details.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the update operation.</returns>
+    public Task<Result<ScheduleResponseDto>> UpdateScheduleAsync(int id, ScheduleRequestDto scheduleRequestDto);
+    /// <summary>
+    /// asynchronously deletes a schedule from the system by their unique identifier.
+    /// </summary>
+    /// <param name="id">the unique identifier of the schedule to delete.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the deletion operation.</returns>
     public Task<Result> DeleteScheduleAsync(int id);
-    public Task<Result<List<ScheduleResponseDto>>> GetAllSchedulesForSpecificEmployee(string employeeId);
+
+    /// <summary>
+    /// asynchronously get all a schedule by agent from the system by their unique identifier.
+    /// </summary>
+    /// <param name="id">the unique identifier of the agent to get all.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the get all schedule by agent operation.</returns>
+
+    public Task<Result<List<ScheduleResponseDto>>> GetScheduleByEmployeeAsync(string id);
 }
