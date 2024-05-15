52c92a05bc9f1db9acfb5a23592a95ee61ec5703
hager3012 <hager3012>
2024-05-15 15:50:56 +0300

    add schedule get by id response dto

---
+++ ServiceCenter.Application/DTOS/ScheduleGetByIdResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS;
+
+public class ScheduleGetByIdResponseDto
+{
+    public int Id { get; set; }
+    public EmployeeResponseDto Employee { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public string Day { get; set; } = "";
+}
