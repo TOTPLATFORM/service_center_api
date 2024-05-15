ebad0f7245da26a599577063ab9ee505ad763381
hager3012 <hager3012>
2024-05-15 15:50:31 +0300

    add appointment response dto

--- ServiceCenter.Application/DTOS/AppointmentResponseDto.cs
+++ ServiceCenter.Application/DTOS/AppointmentResponseDto.cs
@@ -9,10 +9,9 @@ namespace ServiceCenter.Application.DTOS;
 public class AppointmentResponseDto
 {
     public int Id { get; set; }
+    public CustomerResponseDto Customer { get; set; }
+    public EmployeeResponseDto Employee { get; set; }
     public TimeOnly StartTime { get; set; }
     public TimeOnly EndTime { get; set; }
-    public DateOnly Date { get; set; }
-	public string Day { get; set; } = "";
-	public string CustomerName { get; set; } = "";
-    public string EmployeeName { get; set; } = "";
+    public string Day { get; set; } = "";
 }
