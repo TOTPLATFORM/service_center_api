9b60a252c06a65a5d8d439a40a9ee15e4016af20
hager3012 <hager3012>
2024-05-15 15:54:45 +0300

    enhance schedule data test

--- ServiceCenter.Test/TestSetup/Data/ScheduleTest.cs
+++ ServiceCenter.Test/TestSetup/Data/ScheduleTest.cs
@@ -15,17 +15,11 @@ public static class ScheduleTest
         context.Schedules.AddRange(
         new Schedule
         {
-            Id = 1,
-            TimeSlotId = 1,
-            EmployeeId= "53ae72a7-589e-4f0b-81ed-4038169498",
-            AppointmentId=1
+            Id = 1
         },
         new Schedule
         {
-            Id = 2,
-            TimeSlotId = 2,
-            EmployeeId = "53ae72a7-589e-4f0b-81ed-4038169498",
-            AppointmentId = 1
+            Id = 2
         }
         );
     }
