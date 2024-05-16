7c3fe7eed632b348c9be6a809a23cd9a1483d875
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-16 14:45:24 +0300

    add mapping for campagin entity

---
+++ ServiceCenter.API/ExtensionMethods/CampaginMapping.cs
@@ -0,0 +1,15 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods;
+
+public static class CampaginMapping
+{
+	public static void AddCampaginMapping(this MappingProfiles map)
+	{
+		map.CreateMap<CampaginRequestDto, Campagin>();
+
+		map.CreateMap<Campagin, CampaginResponseDto>();
+	}
+}
