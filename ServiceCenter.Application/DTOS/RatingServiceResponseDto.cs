9e1c72a692f92a531801d8162df06c7b63f3434a
hager3012 <hager3012>
2024-05-16 17:04:37 +0300

    add rating service response dto

---
+++ ServiceCenter.Application/DTOS/RatingServiceResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS;
+
+public class RatingServiceResponseDto
+{    public int Id { get; set; }
+    public int RatingValue { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string ServiceName { get; set; } = "";
+    public string CustomerName { get; set; } = "";
+
+}
