79a680a16f2362ffc07d302c0640d513d0504ee5
hager3012 <hager3012>
2024-05-16 17:38:44 +0300

    enhance in login response dto

--- ServiceCenter.Application/DTOS/LoginResponseDto.cs
+++ ServiceCenter.Application/DTOS/LoginResponseDto.cs
@@ -8,7 +8,8 @@ namespace ServiceCenter.Application.DTOS;
 
 public class LoginResponseDto
 {
-	public string UserName { get; set; } = "";
+    public string UserId { get; set; }
+    public string UserName { get; set; } = "";
 	public string FullName { get; set; } = "";
 	public required List<string> Roles { get; set; }
 	public string Token { get; set; } = "";
