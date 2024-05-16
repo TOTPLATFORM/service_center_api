91cddf40a575d4658006a7b513dd9d9a7448ee93
hager3012 <hager3012>
2024-05-16 19:18:54 +0300

    add ware house manager mapping

---
+++ ServiceCenter.API/ExtensionMethods/WareHouseManagerMapping.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods;
+
+public static class WareHouseManagerMapping
+{
+    public static void AddWareHouseManagerMapping(this MappingProfiles map)
+    {
+        map.CreateMap<WareHouseManagerRequestDto, WareHouseManager>();
+        map.CreateMap<WareHouseManager, WareHouseManagerResponseDto>()
+            .ForMember(d => d.WareHouseManagerEmail, o => o.MapFrom(s => s.Email))
+            .ForMember(d => d.WareHouseManagerPhoneNumber, o => o.MapFrom(s => s.PhoneNumber))
+            .ForMember(d => d.WareHouseManagerFirstName, o => o.MapFrom(s => s.FirstName))
+            .ForMember(d => d.WareHouseManagerLastName, o => o.MapFrom(s => s.LastName))
+            .ForMember(d => d.InventoryName, o => o.MapFrom(s => s.Inventory.InventoryName));
+    }
+}
