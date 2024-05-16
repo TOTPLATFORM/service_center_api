3dac28b1c675b580000880354129a974abe78cbd
Salah Bakr <bakrkhaled896@gmail.com>
2024-05-16 13:27:09 +0300

    add sales mapping

---
+++ ServiceCenter.API/ExtensionMethods/SalesMapping.cs
@@ -0,0 +1,25 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
+
+namespace ServiceCenter.API.ExtensionMethods;
+
+public static class SalesMapping
+{
+    public static void AddSalesMapping(this MappingProfiles map)
+    {
+        map.CreateMap<SalesRequestDto, Sales>()
+            .ForMember(dest => dest.FirstName, src => src.MapFrom(src => src.SalesFirstName))
+            .ForMember(dest => dest.LastName, src => src.MapFrom(src => src.SalesLastName))
+            .ForMember(dest => dest.PhoneNumber, src => src.MapFrom(src => src.SalesPhoneNumber))
+            .ForMember(dest => dest.Email, src => src.MapFrom(src => src.SalesEmail))
+            .ReverseMap();
+
+        map.CreateMap<Sales, SalesResponseDto>()
+            .ForMember(dest => dest.SalesFirstName, src => src.MapFrom(src => src.FirstName))
+            .ForMember(dest => dest.SalesLastName, src => src.MapFrom(src => src.LastName))
+            .ForMember(dest => dest.SalesPhoneNumber, src => src.MapFrom(src => src.PhoneNumber))
+            .ForMember(dest => dest.SalesEmail, src => src.MapFrom(src => src.Email))
+            .ReverseMap();
+    }
+}
