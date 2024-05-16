46ebfc2fe8029ecd40eb1f3b4c06a8b167295078
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-16 14:50:47 +0300

    add campagin dtos

---
+++ ServiceCenter.Application/DTOS/CampaginResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using ServiceCenter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS;
+
+public class CampaginResponseDto
+{
+    public int Id { get; set; }
+    public string CamapginName { get; set; } = "";
+	public string CampaginDescription { get; set; } = "";
+	public DateOnly StartDate { get; set; }
+	public DateOnly EndDate { get; set; }
+	public int Budget { get; set; }
+	public string Goals { get; set; } = "";
+	public string Status { get; set; } = "";
+}
