4e462a6507d4461120118e197bb06e7ad5c0fd5b
RadwaaAyman <radwa.ayman@totplatform.com>
2024-05-19 17:40:29 +0300

    add get all campagins fucntion in campagin service , add get campagin by id  fucntion in campagin service , add search campagin by text  fucntion in campagin service , add delete campagin  fucntion in campagin service , add update campagin  fucntion in campagin service , add update campagin status  fucntion in campagin service ,

--- ServiceCenter.Application/Services/CampaginService.cs
+++ ServiceCenter.Application/Services/CampaginService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ServiceCenter.Application.Contracts;
@@ -59,4 +60,139 @@ public class CampaginService(ServiceCenterBaseDbContext dbContext, IMapper mappe
 
 		return Result.SuccessWithMessage("Campagin added successfully");
 	}
+
+
+	///<inheritdoc/>
+	public async Task<Result<List<CampaginResponseDto>>> GetAllCampaginsAsync()
+	{
+		var result = await _dbContext.Campagins
+				 .ProjectTo<CampaginResponseDto>(_mapper.ConfigurationProvider)
+				 .ToListAsync();
+
+		_logger.LogInformation("Fetching all Campagins. Total count: {campagin}.", result.Count);
+
+		return Result.Success(result);
+	}
+
+	///<inheritdoc/>
+	public async Task<Result<CampaginGetByIdResposeDto>> GetCampaginByIdAsync(int id)
+	{
+		var result = await _dbContext.Campagins
+				.ProjectTo<CampaginGetByIdResposeDto>(_mapper.ConfigurationProvider)
+				.FirstOrDefaultAsync(c=> c.Id == id);
+
+		if (result is null)
+		{
+			_logger.LogWarning("Camapgin Id not found,Id {CampaginId}", id);
+
+			return Result.NotFound(["Campagin not found"]);
+		}
+
+		_logger.LogInformation("Fetching Camapgin");
+
+		return Result.Success(result);
+	}
+
+	///<inheritdoc/>
+	public async Task<Result<CampaginGetByIdResposeDto>> UpdateCampaginAsync(int id, CampaginRequestDto campaginRequestDto)
+	{
+		var result = await _dbContext.Campagins.FindAsync(id);
+
+		if (result is null)
+		{
+			_logger.LogWarning("Camapgin Id not found,Id {CampaginId}", id);
+			return Result.NotFound(["Campagin not found"]);
+		}
+
+		result.ModifiedBy = _userContext.Email;
+
+		_mapper.Map(campaginRequestDto, result);
+
+		await _dbContext.SaveChangesAsync();
+
+		var campagin = _mapper.Map<CampaginGetByIdResposeDto>(result);
+
+		if (campagin is null)
+		{
+			_logger.LogError("Failed to map CampaginRequestDto to CampaginResponseDto. CampaginRequestDto: {@CampaginRequestDto}", campagin);
+
+			return Result.Invalid(new List<ValidationError>
+			{
+					new ValidationError
+					{
+						ErrorMessage = "Validation Errror"
+					}
+			});
+		}
+
+		_logger.LogInformation("Updated Campagin , Id {Id}", id);
+
+		return Result.Success(campagin);
+	}
+
+	///<inheritdoc/>
+
+	public async Task<Result<CampaginGetByIdResposeDto>> UpdateCampaginStatusAsync(int id, CampaginStatus status)
+	{
+		var campagins = await _dbContext.Campagins.FindAsync(id);
+
+		if (campagins is null)
+		{
+			_logger.LogWarning($"Campagin with id {id} was not found while attempting to update contact status by id");
+
+			return Result.NotFound(["The contact is not found"]);
+		}
+
+		if (status == CampaginStatus.Cancelled)
+		{
+			_dbContext.Campagins.Remove(campagins);
+			await _dbContext.SaveChangesAsync();
+		}
+
+		var previousCampaginStatus = campagins.Status;
+		campagins.Status = status;
+		campagins.ModifiedBy = _userContext.Email;
+		await _dbContext.SaveChangesAsync();
+
+		var campaginResponseDto = _mapper.Map<CampaginGetByIdResposeDto>(campagins);
+
+		_logger.LogInformation($"Successfully update campagin status to: {campagins.Status} from: {previousCampaginStatus}");
+
+		return Result.Success(campaginResponseDto, "Successfully updated campagin");
+	}
+	///<inheritdoc/>
+	public async Task<Result<List<CampaginResponseDto>>> SearchCampaginByTextAsync(string text)
+	{
+		var campagins = await _dbContext.Campagins
+					   .ProjectTo<CampaginResponseDto>(_mapper.ConfigurationProvider)
+					   .Where(c => c.CamapginName.Contains(text) || c.CampaginDescription.Contains(text))
+					   .ToListAsync();
+
+		_logger.LogInformation("Fetching search campagin by name . Total count: {campagin}.", campagins.Count);
+
+		return Result.Success(campagins);
+	}
+
+	///<inheritdoc/>
+	public async Task<Result> DeleteCampaginAsync(int id)
+	{
+		var campagins = await _dbContext.Campagins.FindAsync(id);
+
+		if (campagins is null)
+		{
+			_logger.LogWarning("Campagin Invaild Id ,Id {CampaginId}", id);
+
+			return Result.NotFound(["Campagin Invaild Id"]);
+		}
+
+		_dbContext.Campagins.Remove(campagins);
+
+		await _dbContext.SaveChangesAsync();
+
+		_logger.LogInformation("Campagin removed successfully in the database");
+
+		return Result.SuccessWithMessage("Campagin removed successfully");
+	}
+
+	
 }
