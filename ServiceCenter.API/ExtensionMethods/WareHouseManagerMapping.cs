f5f91ec9321d6ddc3c695c5d0a0346ced937fbd0
hager3012 <hager3012>
2024-05-20 15:15:23 +0300

    enhance in warehouse manager mapping

--- ServiceCenter.API/ExtensionMethods/WareHouseManagerMapping.cs
+++ ServiceCenter.API/ExtensionMethods/WareHouseManagerMapping.cs
@@ -8,7 +8,11 @@ public static class WareHouseManagerMapping
 {
     public static void AddWareHouseManagerMapping(this MappingProfiles map)
     {
-        map.CreateMap<WareHouseManagerRequestDto, WareHouseManager>();
+        map.CreateMap<WareHouseManagerRequestDto, WareHouseManager>()
+            .ForMember(d => d.Email, o => o.MapFrom(s => s.WareHouseManagerEmail))
+            .ForMember(d => d.PhoneNumber, o => o.MapFrom(s => s.WareHouseManagerPhoneNumber))
+            .ForMember(d => d.FirstName, o => o.MapFrom(s => s.WareHouseManagerFirstName))
+            .ForMember(d => d.LastName, o => o.MapFrom(s => s.WareHouseManagerLastName));
         map.CreateMap<WareHouseManager, WareHouseManagerResponseDto>()
             .ForMember(d => d.WareHouseManagerEmail, o => o.MapFrom(s => s.Email))
             .ForMember(d => d.WareHouseManagerPhoneNumber, o => o.MapFrom(s => s.PhoneNumber))
