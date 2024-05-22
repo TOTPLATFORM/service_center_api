7a9b1cceeb038e68e63f8e4246a4f3645c2a289c
Salah Bakr <bakrkhaled896@gmail.com>
2024-05-22 11:45:46 +0300

    enhance branch service interface

--- ServiceCenter.Application/Contracts/IBranchService.cs
+++ ServiceCenter.Application/Contracts/IBranchService.cs
@@ -10,46 +10,46 @@ namespace ServiceCenter.Application.Contracts;
 
 public interface IBranchService : IApplicationService, IScopedService
 {
-	/// <summary>
-	/// function to add branch that take branchDto   
-	/// </summary>
-	/// <param name="branchRequestDto">time slot request dto</param>
-	/// <returns>Branch added successfully </returns>
+    /// <summary>
+    /// asynchronously adds a new branch to the database.
+    /// </summary>
+    /// <param name="branchRequestDto">the branch data transfer object containing the details necessary for creation.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the branch addition.</returns>
 	public Task<Result> AddBranchAsync(BranchRequestDto branchRequestDto);
 
-	/// <summary>
-	/// function to get all branches 
-	/// </summary>
-	/// <returns>list all branchResponseDto </returns>
+    /// <summary>
+    /// asynchronously retrieves all branchs in the system.
+    /// </summary>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result containing a list of branch response DTOs.</returns>
 	public Task<Result<List<BranchResponseDto>>> GetAllBranchesAsync();
 
-	/// <summary>
-	/// function to get branch by id that take  branch id
-	/// </summary>
-	/// <param name="id">branch id</param>
-	/// <returns>branch response dto</returns>
+    /// <summary>
+    /// asynchronously retrieves a branch by their unique identifier.
+    /// </summary>
+    /// <param name="id">the unique identifier of the branch to retrieve.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result containing the branch response DTO.</returns>
 	public Task<Result<BranchResponseDto>> GetBranchByIdAsync(int id);
 
-	/// <summary>
-	/// function to update branch that take BranchRequestDto   
-	/// </summary>
-	/// <param name="id">branch id</param>
-	/// <param name="BranchRequestDto">branch dto</param>
-	/// <returns>Updated Branch </returns>
-	public Task<Result<BranchResponseDto>> UpdateBranchAsync(int id, BranchRequestDto BranchRequestDto);
+    /// <summary>
+    /// asynchronously updates the data of an existing branch.
+    /// </summary>
+    /// <param name="id">the unique identifier of the branch to update.</param>
+    /// <param name="branchRequestDto">the branch data transfer object containing the updated details.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the update operation.</returns>
+	public Task<Result<BranchResponseDto>> UpdateBranchAsync(int id, BranchRequestDto branchRequestDto);
 
 
-	/// <summary>
-	/// function to search branch by text  that take text   
-	/// </summary>
-	/// <param name="text">text</param>
-	/// <returns>all branches that contain this text </returns>
+    /// <summary>
+    /// asynchronously searches for branchs based on the provided text.
+    /// </summary>
+    /// <param name="text">the text to search within branch data.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result containing a list of branch response DTOs that match the search criteria.</returns>
 	public Task<Result<List<BranchResponseDto>>> SearchBranchByTextAsync(string text);
 
-	/// <summary>
-	/// function to delete Branch that take BranchDto   
-	/// </summary>
-	/// <param name="id">time slot id</param>
-	/// <returns>Branch removed successfully </returns>
+    /// <summary>
+    /// asynchronously deletes a branch from the system by their unique identifier.
+    /// </summary>
+    /// <param name="id">the unique identifier of the branch to delete.</param>
+    /// <returns>a task that represents the asynchronous operation, which encapsulates the result of the deletion operation.</returns>
 	public Task<Result> DeleteBranchAsync(int id);
 }
