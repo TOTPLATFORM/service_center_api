cc9f6f42403ca16c5eb2d094286171e05b338c89
MariamAbdeen <MariamAbdeen299@gmail.com>
2024-06-03 18:24:46 +0300

    add add transaction function in transaction service

---
+++ ServiceCenter.Application/Contracts/ITransactionService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Contracts;
+
+public interface ITransactionService : IApplicationService, IScopedService
+{
+    /// <summary>
+    /// function to add transaction that take transactionDto   
+    /// </summary>
+    /// <param name="TransactionRequestDto">transaction request dto</param>
+    /// <returns>Transaction added successfully </returns>
+    public Task<Result> AddTransactionAsync(TransactionRequestDto transactionRequestDto);
+}
---
+++ ServiceCenter.Application/Services/TransactionService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using ServiceCenter.Application.Contracts;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Core.Result;
+using ServiceCenter.Domain.Entities;
+using ServiceCenter.Infrastructure.BaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.Services;
+
+public class TransactionService(ServiceCenterBaseDbContext dbContext, IMapper mapper, ILogger<TransactionService> logger, IUserContextService userContext) : ITransactionService
+{
+    private readonly ServiceCenterBaseDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<TransactionService> _logger = logger;
+    private readonly IUserContextService _userContext = userContext;
+
+
+    ///<inheritdoc/>
+    public async Task<Result> AddTransactionAsync(TransactionRequestDto transactionRequestDto)
+    {
+        var result = _mapper.Map<Transaction>(transactionRequestDto);
+
+        if (result is null)
+        {
+            _logger.LogError("Failed to map TransactionRequestDto to Transaction. TransactionRequestDto: {@TransactionRequestDto}", transactionRequestDto);
+
+            return Result.Invalid(new List<ValidationError>
+        {
+            new ValidationError
+            {
+                ErrorMessage = "Validation Errror"
+            }
+        });
+        }
+        result.CreatedBy = _userContext.Email;
+        _dbContext.Transactions.Add(result);
+
+        await _dbContext.SaveChangesAsync();
+
+        _logger.LogInformation("Transaction added successfully to the database");
+
+        return Result.SuccessWithMessage("Transaction added successfully");
+    }
+
+}
