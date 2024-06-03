0cdb724bc9c2148506dd9749b0094d738c4421de
MariamAbdeen <MariamAbdeen299@gmail.com>
2024-06-03 18:11:35 +0300

    add transaction configuration

---
+++ ServiceCenter.Infrastructure/Configuration/TransactionConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServiceCenter.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Infrastructure.Configuration;
+
+public class TransactionConfigurations:IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.Property(p => p.TransactionType)
+               .IsRequired();
+        builder.Property(p => p.Quantity)
+              .IsRequired();
+        builder.Property(p => p.TransactionDate)
+              .IsRequired();
+
+    }
+}
