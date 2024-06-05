99bcb30825b8c752cfc94ffcfe747edd00662e0d
hager3012 <hager3012>
2024-06-05 14:04:34 +0300

    enhance in ceneter response dto

--- ServiceCenter.Application/DTOS/CenterResponseDto.cs
+++ ServiceCenter.Application/DTOS/CenterResponseDto.cs
@@ -13,11 +13,4 @@ public class CenterResponseDto
 	public string CenterName { get; set; } = "";
 	public int OpeningHours { get; set; }
 	public string Specialty { get; set; } = "";
-	public  ICollection<BranchResponseDto> Branches { get; set; } = new HashSet<BranchResponseDto>();
-	public  ICollection<DepartmentResponseDto> Departments { get; set; } = new HashSet<DepartmentResponseDto>();
-
-    public  ICollection<ServiceResponseDto> Services { get; set; } = new HashSet<ServiceResponseDto>();
-    public  ICollection<SalesResponseDto?> Sales { get; set; } = new HashSet<SalesResponseDto?>();
-    public  ICollection<CampaginResponseDto?> Campagins { get; set; } = new HashSet<CampaginResponseDto?>();
-    public  ICollection<VendorResponseDto?> Vendors { get; set; } = new HashSet<VendorResponseDto?>();
 }
