580316900478823ada1fb735799ab1e2bd24001e
hager3012 <hager3012>
2024-06-06 17:45:21 +0300

    add service provider response dto

--- ServiceCenter.Application/DTOS/ServiceProviderResponseDto.cs
+++ ServiceCenter.Application/DTOS/ServiceProviderResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceCenter.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +9,11 @@ namespace ServiceCenter.Application.DTOS;
 
 public class ServiceProviderResponseDto
 {
+    public string Id { get; set; } = "";
+    public string ServiceProviderEmail { get; set; } = "";
+    public string ServiceProviderFirstName { get; set; } = "";
+    public string ServiceProviderLastName { get; set; } = "";
+    public string ServiceProviderPhoneNumber { get; set; } = "";
+    public Gender Gender { get; set; }
+    public string UserName { get; set; } = "";
 }
