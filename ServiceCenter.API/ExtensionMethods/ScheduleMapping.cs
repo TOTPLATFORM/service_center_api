ef516437fe0520d33f0c4320a08b8d166879aa62
hager3012 <hager3012>
2024-06-06 14:48:53 +0300

    enhnace in schedule mapping

--- ServiceCenter.API/ExtensionMethods/ScheduleMapping.cs
+++ ServiceCenter.API/ExtensionMethods/ScheduleMapping.cs
@@ -1,24 +1,15 @@
-﻿//using ServiceCenter.API.Mapping;
-//using ServiceCenter.Application.DTOS;
-//using ServiceCenter.Domain.Entities;
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+using ServiceCenter.Domain.Entities;
 
-//namespace ServiceCenter.API.ExtensionMethods;
+namespace ServiceCenter.API.ExtensionMethods;
 
-//public static class ScheduleMapping
-//{
-//    public static void AddScheduleMapping(this MappingProfiles map)
-//    {
-//        map.CreateMap<ScheduleRequestDto, Schedule>().ReverseMap();
-//        map.CreateMap<Schedule, ScheduleResponseDto>()
-//           .ForMember(dest => dest.EmployeeName, src => src.MapFrom(src => src.Employee.FirstName))
-//           .ForMember(dest => dest.StartTime, src => src.MapFrom(src => src.TimeSlot.StartTime))
-//           .ForMember(dest => dest.EndTime, src => src.MapFrom(src => src.TimeSlot.EndTime))
-//           .ForMember(dest => dest.Day, src => src.MapFrom(src => src.TimeSlot.Day))
-//            .ReverseMap();
-
-//        map.CreateMap<Schedule, ScheduleGetByIdResponseDto>()
-//            .ForMember(d => d.StartTime, o => o.MapFrom(s => s.TimeSlot.StartTime))
-//            .ForMember(d => d.EndTime, o => o.MapFrom(s => s.TimeSlot.EndTime))
-//            .ForMember(d => d.Day, o => o.MapFrom(s => s.TimeSlot.Day));
-//    }
-//}
+public static class ScheduleMapping
+{
+    public static void AddScheduleMapping(this MappingProfiles map)
+    {
+        map.CreateMap<ScheduleRequestDto, Schedule>().ReverseMap();
+        map.CreateMap<Schedule, ScheduleResponseDto>()
+            .ForMember(dest => dest.ServiceProviderName, src => src.MapFrom(src => src.ServiceProvider.FirstName + " " + src.ServiceProvider.LastName));
+    }
+}
