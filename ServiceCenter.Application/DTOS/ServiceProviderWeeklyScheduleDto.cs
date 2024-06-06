2d8005ecd39a0faeb613b93b9021ce89f69b33c0
hager3012 <hager3012>
2024-06-06 14:53:01 +0300

    add in service provider weekly schedule dto

---
+++ ServiceCenter.Application/DTOS/ServiceProviderWeeklyScheduleDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Application.DTOS;
+
+public class ServiceProviderWeeklyScheduleDto
+{
+    public string ServiceProviderId { get; set; } = "";
+    public string ServiceProviderName { get; set; } = "";
+    public DayOfWeek DayOfWeek { get; set; }
+    public DateTime Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+}
