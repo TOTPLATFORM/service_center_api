a21faa1fb4d415ed96c4a1235b8b9cf46daef4f0
hager3012 <hager3012>
2024-06-06 17:43:57 +0300

    add service provider mapping

---
+++ ServiceCenter.API/ExtensionMethods/ServiceProviderMapping.cs
@@ -0,0 +1,19 @@
+﻿using ServiceCenter.API.Mapping;
+using ServiceCenter.Application.DTOS;
+namespace ServiceCenter.API.ExtensionMethods;
+
+public static class ServiceProviderMapping
+{
+    public static void AddServiceProviderMapping(this MappingProfiles map)
+    {
+        map.CreateMap<ServiceProviderRequestDto,Domain.Entities.ServiceProvider>()
+            .ForMember(dest => dest.PhoneNumber, src => src.MapFrom(src => src.Phone));
+
+        map.CreateMap<Domain.Entities.ServiceProvider, ServiceProviderResponseDto>()
+            .ForMember(dest => dest.ServiceProviderFirstName, src => src.MapFrom(src => src.FirstName))
+            .ForMember(dest => dest.ServiceProviderLastName, src => src.MapFrom(src => src.LastName))
+            .ForMember(dest => dest.ServiceProviderPhoneNumber, src => src.MapFrom(src => src.PhoneNumber))
+            .ForMember(dest => dest.ServiceProviderEmail, src => src.MapFrom(src => src.Email));
+
+    }
+}
