bbf030798ccb30bb112ae43da34b83b905e89fae
hager3012 <hager3012>
2024-06-06 14:50:56 +0300

    enhance in appointment response dto

--- ServiceCenter.Application/DTOS/AppointmentResponseDto.cs
+++ ServiceCenter.Application/DTOS/AppointmentResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceCenter.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,12 @@ namespace ServiceCenter.Application.DTOS;
 public class AppointmentResponseDto
 {
     public int Id { get; set; }
- //   public CustomerResponseDto Customer { get; set; }
-    public EmployeeResponseDto Employee { get; set; }
     public TimeOnly StartTime { get; set; }
     public TimeOnly EndTime { get; set; }
-    public string Day { get; set; } = "";
+    public DateTime AppointmentDate { get; set; }
+    public AppointmentStatus Status { get; set; }
+    public string Description { get; set; } = "";
+    public ContactResponseDto Contact { get; set; }
+    public ServiceProviderResponseDto ServiceProvider { get; set; }
+    public int ScheduleId { get; set; }
 }
