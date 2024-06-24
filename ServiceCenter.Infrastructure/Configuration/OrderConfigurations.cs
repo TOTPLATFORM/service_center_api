e4bb3038f9d84dfa17ae3de1efc2b5fb093b88e5
hager3012 <hager3012>
2024-06-24 17:37:29 +0300

    edit order configuration

--- ServiceCenter.Infrastructure/Configuration/OrderConfigurations.cs
+++ ServiceCenter.Infrastructure/Configuration/OrderConfigurations.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ServiceCenter.Domain.Entities;
+﻿//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using ServiceCenter.Domain.Entities;
 
-namespace ServiceCenter.Infrastructure.Configuration;
+//namespace ServiceCenter.Infrastructure.Configuration;
 
-public class OrderConfigurations : IEntityTypeConfiguration<Order>
-{
-    public void Configure(EntityTypeBuilder<Order> builder)
-    {
-        builder.Property(p => p.From)
-               .IsRequired()
-               .HasColumnType("varchar")
-              .HasMaxLength(50);
+//public class OrderConfigurations : IEntityTypeConfiguration<Order>
+//{
+//    public void Configure(EntityTypeBuilder<Order> builder)
+//    {
+//        builder.Property(p => p.From)
+//               .IsRequired()
+//               .HasColumnType("varchar")
+//              .HasMaxLength(50);
 
-        builder.Property(p => p.OrderArrivalDate)
-               .IsRequired();
+//        builder.Property(p => p.OrderArrivalDate)
+//               .IsRequired();
 
-        builder.Property(p => p.OrderDate)
-              .IsRequired();
-        builder.Property(p => p.OrderStatus)
-          .IsRequired();
+//        builder.Property(p => p.OrderDate)
+//              .IsRequired();
+//        builder.Property(p => p.OrderStatus)
+//          .IsRequired();
 
-    }
-}
+//    }
+//}
